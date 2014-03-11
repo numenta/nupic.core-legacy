ecd6ba76783c7f3cf2bd4bacce760bc37c17007f
Marek Otahal <markotahal@gmail.com>
2014-03-11 02:43:29 -0700

    Revert "use POSIX version of esrerror_r"
    This reverts commit faa7ac7224a9a616bacec0c034455d156aabdc96.

--- os/FStream.cpp
+++ os/FStream.cpp
@@ -183,11 +183,9 @@ void *ZLib::fopen(const std::string &filename, const std::string &mode,
           case Z_VERSION_ERROR: message = "Zlib version error."; break;
           default:
             char errbuf[256];
-            int retval = ::strerror_r(error, errbuf, 256);
-            if(retval != 0) {
+            char* retval = ::strerror_r(error, errbuf, 256);
+            if(retval != NULL) {
                 //empty code to silence -Werror=unused-variable warning
-                std::cerr << "Error: Failed function strerror_r " << std::endl;
-                exit(1);
             }
             message = errbuf;
             break;
