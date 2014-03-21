513af1400a0f9c47d09e36f24588fbe219ea66db
Matthew Taylor <rhyolight@gmail.com>
2014-03-21 13:55:21 -0700

    Fix edge case where initial connected percent is 1

--- algorithms/spatial_pooler.cpp
+++ algorithms/spatial_pooler.cpp
@@ -624,7 +624,7 @@ vector<Real> SpatialPooler::initPermanence_(vector<UInt>& potential,
       continue;
     }
 
-    if (rng_.getReal64() < connectedPct) {
+    if (rng_.getReal64() <= connectedPct) {
       perm[i] = initPermConnected_();
     } else {
       perm[i] = initPermNonConnected_();
