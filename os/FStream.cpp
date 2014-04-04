9621f42c95f820eb4ffe3ec88a4d13ddf4dd1e75
Marek Otahal <markotahal@gmail.com>
2014-04-04 11:44:17 -0700

    Revert "Revert "use POSIX version of esrerror_r""
    This reverts commit ecd6ba76783c7f3cf2bd4bacce760bc37c17007f.
    
    Conflicts:
    	os/FStream.cpp
    
    Because it introduced problems on OSX
    Use strerror only, simplify

--- os/FStream.cpp
+++ os/FStream.cpp
@@ -182,12 +182,7 @@ void *ZLib::fopen(const std::string &filename, const std::string &mode,
           case Z_BUF_ERROR: message = "Zlib buffer error."; break;
           case Z_VERSION_ERROR: message = "Zlib version error."; break;
           default:
-            char errbuf[256];
-            char* retval = (char*)::strerror_r(error, errbuf, 256);
-            if(retval != NULL) {
-                //empty code to silence -Werror=unused-variable warning
-            }
-            message = errbuf;
+            message = ::strerror(error);
             break;
         }   
         if(errorMessage) {
