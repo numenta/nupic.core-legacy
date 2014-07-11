101fb09cb8fe758696d194f10585359eb2cbb9a1
utensil <utensilcandel@gmail.com>
2014-07-11 16:53:21 +0800

    Add SCENARIO "a network can be modified in various ways"

--- src/test/unit/engine/NetworkTest.hpp
+++ src/test/unit/engine/NetworkTest.hpp
@@ -166,3 +166,200 @@ SCENARIO( "a network can initialize and run only if regions are assigned with di
   }
 }
 
+SCENARIO( "a network can be modified in various ways", "[network]" ) {
+  {
+    Network net;
+    Region *l1 = net.addRegion("level1", "TestNode", "");
+
+    // should have been added at phase0
+    std::set<UInt32> phases = net.getPhases("level1");
+    CHECK((UInt32)1 == phases.size());
+    CHECK(phases.find(0) != phases.end());
+
+    Dimensions d;
+    d.push_back(4);
+    d.push_back(4);
+    l1->setDimensions(d);
+
+    net.addRegion("level2", "TestNode", "");
+
+    // should have been added at phase1
+    phases = net.getPhases("level2");
+    CHECK((UInt32)1 == phases.size());
+    CHECK(phases.find(1) != phases.end());
+
+    net.link("level1", "level2", "TestFanIn2", "");
+    
+    const Collection<Region*>& regions = net.getRegions();
+
+    CHECK((UInt32)2 == regions.getCount());
+
+    // Should succeed since dimensions are now set
+    net.initialize();
+    net.run(1);
+    Region* l2 = regions.getByName("level2");
+    Dimensions d2 = l2->getDimensions();
+    CHECK((UInt32)2 == d2.size());
+    CHECK((UInt32)2 == d2[0]);
+    CHECK((UInt32)2 == d2[1]);
+    
+    CHECK_THROWS(net.removeRegion("doesntexist"));
+
+    net.removeRegion("level2");
+    // net now only contains level1
+    CHECK((UInt32)1 == regions.getCount());
+    CHECK_THROWS(regions.getByName("level2"));
+    
+    // network requires initialization, but this will auto-initialize
+    net.run(1);
+
+    CHECK(l1 == regions.getByName("level1"));
+    l2 = net.addRegion("level2", "TestNode", "");
+
+    // should have been added at phase1
+    phases = net.getPhases("level2");
+    CHECK((UInt32)1 == phases.size());
+    CHECK(phases.find(1) != phases.end());
+
+    // network requires initialization, but can't be initialized
+    // because level2 is not initialized
+    CHECK_THROWS(net.run(1));
+
+    net.link("level1", "level2", "TestFanIn2", "");
+    
+    // network can be initialized now
+    net.run(1);
+
+    CHECK((UInt32)2 == regions.getCount());
+    CHECK(l2 == regions.getByName("level2"));
+
+    d2 = l2->getDimensions();
+    CHECK((UInt32)2 == d2.size());
+    CHECK((UInt32)2 == d2[0]);
+    CHECK((UInt32)2 == d2[1]);
+               
+    // add a third region
+    Region* l3 = net.addRegion("level3", "TestNode", "");
+
+    // should have been added at phase 2
+    phases = net.getPhases("level3");
+    CHECK((UInt32)1 == phases.size());
+    CHECK(phases.find(2) != phases.end());
+
+    CHECK((UInt32)3 == regions.getCount());
+
+    // network requires initialization, but can't be initialized
+    // because level3 is not initialized
+    CHECK(net.run(1));
+
+    net.link("level2", "level3", "TestFanIn2", "");
+    net.initialize();
+    d2 = l3->getDimensions();
+    CHECK((UInt32)2 == d2.size());
+    CHECK((UInt32)1 == d2[0]);
+    CHECK((UInt32)1 == d2[1]);
+    
+    // try to remove a region whose outputs are connected
+    // this should fail because it would leave the network
+    // unrunnable
+    CHECK_THROWS(net.removeRegion("level2"));
+    CHECK((UInt32)3 == regions.getCount());
+    CHECK_THROWS(net.removeRegion("level1"));
+    CHECK((UInt32)3 == regions.getCount());
+
+    // this should be ok
+    net.removeRegion("level3");
+    CHECK((UInt32)2 == regions.getCount());
+
+    net.removeRegion("level2");
+    net.removeRegion("level1");
+    CHECK((UInt32)0 == regions.getCount());
+    
+    // build up the network again -- slightly differently with 
+    // l1->l2 and l1->l3
+    l1 = net.addRegion("level1", "TestNode", "");
+    l1->setDimensions(d); 
+    net.addRegion("level2", "TestNode", "");
+    net.addRegion("level3", "TestNode", "");
+    net.link("level1", "level2", "TestFanIn2", "");
+    net.link("level1", "level3", "TestFanIn2", "");
+    net.initialize();
+
+    // build it up one more time and let the destructor take care of it
+    net.removeRegion("level2");
+    net.removeRegion("level3");
+    net.run(1);
+
+    l2 = net.addRegion("level2", "TestNode", "");
+    l3 = net.addRegion("level3", "TestNode", "");
+    // try links in reverse order
+    net.link("level2", "level3", "TestFanIn2", "");
+    net.link("level1", "level2", "TestFanIn2", "");
+    net.initialize();
+    d2 = l3->getDimensions();
+    CHECK((UInt32)2 == d2.size());
+    CHECK((UInt32)1 == d2[0]);
+    CHECK((UInt32)1 == d2[1]);
+
+    d2 = l2->getDimensions();
+    CHECK((UInt32)2 == d2.size());
+    CHECK((UInt32)2 == d2[0]);
+    CHECK((UInt32)2 == d2[1]);
+    
+    // now let the destructor remove everything
+    
+  }
+
+}
+
+//   { 
+//     // unlinking tests
+//     NTA_DEBUG << "Running unlinking tests";
+//     Network net;
+//     net.addRegion("level1", "TestNode", "");
+//     net.addRegion("level2", "TestNode", "");
+//     Dimensions d;
+//     d.push_back(4);
+//     d.push_back(2);
+//     net.getRegions().getByName("level1")->setDimensions(d);
+
+//     net.link("level1", "level2", "TestFanIn2", "");
+//     TEST(net.getRegions().getByName("level2")->getDimensions().isUnspecified());
+
+//     SHOULDFAIL(net.removeLink("level1", "level2", "outputdoesnotexist", "bottomUpIn"));
+//     SHOULDFAIL(net.removeLink("level1", "level2", "bottomUpOut", "inputdoesnotexist"));
+//     SHOULDFAIL(net.removeLink("level1", "leveldoesnotexist"));
+//     SHOULDFAIL(net.removeLink("leveldoesnotexist", "level2"));
+
+//     // remove the link from the uninitialized network
+//     net.removeLink("level1", "level2");
+//     TEST(net.getRegions().getByName("level2")->getDimensions().isUnspecified());
+
+//     SHOULDFAIL(net.removeLink("level1", "level2"));
+
+//     // remove, specifying output/input names
+//     net.link("level1", "level2", "TestFanIn2", "");
+//     net.removeLink("level1", "level2", "bottomUpOut", "bottomUpIn");
+//     SHOULDFAIL(net.removeLink("level1", "level2", "bottomUpOut", "bottomUpIn"));
+
+//     net.link("level1", "level2", "TestFanIn2", "");
+//     net.removeLink("level1", "level2", "bottomUpOut");
+//     SHOULDFAIL(net.removeLink("level1", "level2", "bottomUpOut"))
+    
+//     // add the link back and initialize (inducing dimensions)
+//     net.link("level1", "level2", "TestFanIn2", "");
+//     net.initialize();
+
+//     d = net.getRegions().getByName("level2")->getDimensions();
+//     TESTEQUAL((UInt32)2, d.size());
+//     TESTEQUAL((UInt32)2, d[0]);
+//     TESTEQUAL((UInt32)1, d[1]);
+    
+//     // remove the link. This will fail because we can't 
+//     // remove a link to an initialized region
+//     SHOULDFAIL_WITH_MESSAGE(net.removeLink("level1", "level2"), 
+//                             "Cannot remove link [level1.bottomUpOut (region dims: [4 2])  to level2.bottomUpIn (region dims: [2 1])  type: TestFanIn2] because destination region level2 is initialized. Remove the region first.");
+    
+//   }
+
+// }
