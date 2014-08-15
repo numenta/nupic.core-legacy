408f418bfd01a2b9cf8ca1f88563bc2f72d58956
Austin Marshall <amarshall@numenta.com>
2014-08-15 15:07:11 -0700

    Comments and cleanup

--- src/examples/regions/helloregions.cpp
+++ src/examples/regions/helloregions.cpp
@@ -33,20 +33,25 @@ using namespace nta;
 
 int main(int argc, const char * argv[])
 {
+    // Create network
     Network net = Network();
+
+    // Add VectorFileSensor region to network
     Region* region = net.addRegion("region", "VectorFileSensor", "{activeOutputCount: 1}");
-    
+
+    // Set region dimensions
     Dimensions dims;
     dims.push_back(1);
-    
+
     std::cout << "Setting region dimensions" << dims.toString() << std::endl;
-    
+
     region->setDimensions(dims);
 
+    // Load data
     std::string path = Path::makeAbsolute("../../../src/examples/regions/data.csv");
- 
+
     std::cout << "Loading data from " << path << std::endl;
-    
+
     std::vector<std::string> loadFileArgs;
     loadFileArgs.push_back("loadFile");
     loadFileArgs.push_back(path);
@@ -54,22 +59,25 @@ int main(int argc, const char * argv[])
 
     region->executeCommand(loadFileArgs);
 
+    // Initialize network
     std::cout << "Initializing network" << std::endl;
 
     net.initialize();
-    
+
     ArrayRef outputArray = region->getOutputData("dataOut");
-    
+
+    // Compute
     std::cout << "Compute" << std::endl;
-    
+
     region->compute();
 
+    // Get output
     Real64 *buffer = (Real64*) outputArray.getBuffer();
-    
+
     for (size_t i = 0; i < outputArray.getCount(); i++)
     {
         std::cout << "  " << i << "    " << buffer[i] << "" << std::endl;
     }
-    
+
     return 0;
 }
