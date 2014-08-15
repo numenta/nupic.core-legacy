b1ca4eb6cfb6b44e5c592198ea28a2759c4e8f65
Austin Marshall <amarshall@numenta.com>
2014-08-15 14:33:33 -0700

    Simple c++-only region example.

---
+++ src/examples/regions/helloregions.cpp
@@ -0,0 +1,75 @@
+/* ---------------------------------------------------------------------
+ * Numenta Platform for Intelligent Computing (NuPIC)
+ * Copyright (C) 2013-2014, Numenta, Inc.  Unless you have an agreement
+ * with Numenta, Inc., for a separate license for this software code, the
+ * following terms and conditions apply:
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ *
+ * http://numenta.org/licenses/
+ * ---------------------------------------------------------------------
+ */
+
+#include <iostream>
+#include <nta/engine/Network.hpp>
+#include <nta/engine/Region.hpp>
+#include <nta/ntypes/Dimensions.hpp>
+#include <nta/ntypes/ArrayRef.hpp>
+#include <nta/OS/Path.hpp>
+
+
+
+using namespace nta;
+
+int main(int argc, const char * argv[])
+{
+    Network net = Network();
+    Region* region = net.addRegion("region", "VectorFileSensor", "{activeOutputCount: 1}");
+    
+    Dimensions dims;
+    dims.push_back(1);
+    
+    std::cout << "Setting region dimensions" << dims.toString() << std::endl;
+    
+    region->setDimensions(dims);
+
+    std::string path = Path::makeAbsolute("../../../src/examples/regions/data.csv");
+ 
+    std::cout << "Loading data from " << path << std::endl;
+    
+    std::vector<std::string> loadFileArgs;
+    loadFileArgs.push_back("loadFile");
+    loadFileArgs.push_back(path);
+    loadFileArgs.push_back("2");
+
+    region->executeCommand(loadFileArgs);
+
+    std::cout << "Initializing network" << std::endl;
+
+    net.initialize();
+    
+    ArrayRef outputArray = region->getOutputData("dataOut");
+    
+    std::cout << "Compute" << std::endl;
+    
+    region->compute();
+
+    Real64 *buffer = (Real64*) outputArray.getBuffer();
+    
+    for (size_t i = 0; i < outputArray.getCount(); i++)
+    {
+        std::cout << "  " << i << "    " << buffer[i] << "" << std::endl;
+    }
+    
+    return 0;
+}
