6d5e1fabf92e89f7bcdb34a886aee506f42a4901
Richard Crowder <riccro@gmail.com>
2014-10-22 08:13:44 +0100

    CMake tweak for GCC under Windows

--- src/main/types/Types.h
+++ src/main/types/Types.h
@@ -36,6 +36,8 @@
 #if (defined(NTA_PLATFORM_win32) || defined(NTA_PLATFORM_win64)) && defined(NTA_COMPILER_MSVC) && defined(NDEBUG)
 #pragma warning( disable : 4244 ) // conversion from 'double' to 'nta::Real', possible loss of data (LOTS of various type combinations)
 #pragma warning( disable : 4305 ) // truncation from 'double' to 'nta::Real', possible loss of data (LOTS of various type combinations)
+#pragma warning( disable : 4251 ) // needs to have dll-interface to be used by clients of class 
+#pragma warning( disable : 4275 ) // non dll-interface struct used as base for dll-interface class
 #endif
 
 /*---------------------------------------------------------------------- */
