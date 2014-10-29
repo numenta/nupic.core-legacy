2ec15010cfe60bb1be2b47f61881d9018d69bfe5
Chetan Surpur <chetan51@gmail.com>
2014-10-29 14:52:39 -0700

    Started Connections class

---
+++ src/main/algorithms/Connections.hpp
@@ -0,0 +1,75 @@
+/* ---------------------------------------------------------------------
+ * Numenta Platform for Intelligent Computing (NuPIC)
+ * Copyright (C) 2014, Numenta, Inc.  Unless you have an agreement
+ * with Numenta, Inc., for a separate license for this software code, the
+ * following terms and conditions apply:
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ *
+ * http://numenta.org/licenses/
+ * ----------------------------------------------------------------------
+ */
+
+/** @file
+ * Definitions for the Connections class in C++
+ */
+
+#ifndef NTA_CONNECTIONS_HPP
+#define NTA_CONNECTIONS_HPP
+
+#include <nta/types/Types.hpp>
+
+namespace nta
+{
+
+  namespace algorithms
+  {
+
+    namespace connections
+    {
+
+      /**
+       * Connections implementation in C++.
+       *
+       * @b Description
+       * The Connections class is a data structure that represents the
+       * connections of a collection of cells. It is used in the HTM
+       * learning algorithms to store and access data related to the
+       * connectivity of cells.
+       * 
+       */
+      class Connections
+      {
+      public:
+        Connections();
+
+        virtual ~Connections() {}
+
+        /**
+         Creates a segment on the specified cell.
+
+         @param cell Index of cell to create segment on.
+
+         @retval Segment index.
+        */
+        UInt createSegment(UInt cell);
+
+      }; // end class Connections
+
+    } // end namespace connections
+
+  } // end namespace algorithms
+
+} // end namespace nta
+
+#endif // NTA_CONNECTIONS_HPP
