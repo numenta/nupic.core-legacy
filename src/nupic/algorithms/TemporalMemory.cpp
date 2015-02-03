e08b976606e698db52b6d69402c1405e0182e590
Richard Crowder <riccro@gmail.com>
2015-02-03 21:08:04 +0000

    Build fix

--- src/nupic/algorithms/TemporalMemory.cpp
+++ src/nupic/algorithms/TemporalMemory.cpp
@@ -367,11 +367,11 @@ void TemporalMemory::computePredictiveCells(
   vector<Cell>& activeCells,
   Connections& connections)
 {
+/*
 	map<Segment, Int> numActiveConnectedSynapsesForSegment;
 
 	for (Cell cell : activeCells)
 	{
-/*
     for (Synapse synapse : connections.synapsesForPresynapticCell(cell).values())
 		{
 			Segment segment = synapse.segment;
@@ -388,8 +388,8 @@ void TemporalMemory::computePredictiveCells(
 				}
 			}
 		}
-*/
 	}
+*/
 
   return;// make_tuple(activeSegments, predictiveCells);
 }
@@ -747,7 +747,7 @@ bool TemporalMemory::_validateColumn(Int column)
  */
 bool TemporalMemory::_validateCell(Cell& cell)
 {
-  if (cell.idx >= (UInt32)numberOfCells() || cell.idx < 0)
+  if (cell.idx >= (UInt32)numberOfCells())
   {
     NTA_THROW << "Invalid cell " << cell.idx;
     return false;
