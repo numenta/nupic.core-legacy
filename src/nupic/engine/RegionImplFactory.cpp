ecb093396465cf7fb5ed44fa83a61eee5d6debef
Jared Weiss <jweiss@numenta.com>
2015-07-08 14:50:35 -0700

    Throw an error if attempting to register a (python) region that already exists. Warningif registering a (cpp) region that already exists.

--- src/nupic/engine/RegionImplFactory.cpp
+++ src/nupic/engine/RegionImplFactory.cpp
@@ -59,6 +59,12 @@ namespace nupic
   // Allows the user to add custom regions
   void RegionImplFactory::registerPyRegion(const std::string module, const std::string className)
   {
+    // Verify that no regions exist with the same className
+    for (auto pyr=pyRegions.begin(); pyr!=pyRegions.end(); pyr++)
+      if (pyr->second.find(className) != pyr->second.end())
+        NTA_THROW << "A pyRegion with name '" << className << "' already exists. " <<
+        "Unregister the existing region or register the new region using a different name.";
+
     // Module hasn't been added yet
     if (pyRegions.find(module) == pyRegions.end())
     {
@@ -70,6 +76,8 @@ namespace nupic
 
   void RegionImplFactory::registerCPPRegion(const std::string name, GenericRegisteredRegionImpl * wrapper)
   {
+    if (cppRegions.find(name) != cppRegions.end())
+      NTA_WARN << "A CPPRegion already exists with the name '" << name << "'. Overwriting it...";
     cppRegions[name] = wrapper;
   }
 
