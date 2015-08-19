f10c0fb85e2c7e3f8f6433d0f02b1a6adef8b8d8
Matthew Taylor <matt@numenta.org>
2015-08-19 14:20:14 -0700

    AWS cred update. bindings version bump.

--- bindings/py/setup.py
+++ bindings/py/setup.py
@@ -555,7 +555,7 @@ if __name__ == "__main__":
     setup(
       name="nupic.bindings",
       ext_modules=extensions,
-      version="0.1.3",
+      version="0.1.4",
       namespace_packages=["nupic", "nupic.bindings"],
       install_requires=findRequirements(),
       description="Numenta Platform for Intelligent Computing - bindings",
