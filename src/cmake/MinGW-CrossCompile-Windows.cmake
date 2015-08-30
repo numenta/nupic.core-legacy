3e68777fd8e1c46ee5198820809a3996c7305531
Richard Crowder <riccro@gmail.com>
2015-08-30 20:00:11 +0100

    Find AR, NM, RANLIB for CMake

--- src/cmake/MinGW-CrossCompile-Windows.cmake
+++ src/cmake/MinGW-CrossCompile-Windows.cmake
@@ -18,12 +18,12 @@ SET(CMAKE_CROSSCOMPILING 1)
 set(COMPILER_PREFIX "x86_64-w64-mingw32")
 
 # which compilers to use for C and C++
-find_program(CMAKE_RC_COMPILER NAMES ${COMPILER_PREFIX}-windres)
-#SET(CMAKE_RC_COMPILER ${COMPILER_PREFIX}-windres)
+find_program(CMAKE_RC_COMPILER NAMES windres)
 find_program(CMAKE_C_COMPILER NAMES ${COMPILER_PREFIX}-gcc)
-#SET(CMAKE_C_COMPILER ${COMPILER_PREFIX}-gcc)
 find_program(CMAKE_CXX_COMPILER NAMES ${COMPILER_PREFIX}-g++)
-#SET(CMAKE_CXX_COMPILER ${COMPILER_PREFIX}-g++)
+find_program(CMAKE_AR ${COMPILER_PREFIX}-gcc-ar)
+find_program(CMAKE_NM ${COMPILER_PREFIX}-gcc-nm)
+find_program(CMAKE_RANLIB ${COMPILER_PREFIX}-gcc-ranlib)
 
 # Target environment located
 #SET(USER_ROOT_PATH /home/travis/build/rcrowder/travis-ci-test)
