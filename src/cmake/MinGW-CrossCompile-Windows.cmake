0a930a52dcd1b71dc9ef6c91ca980d3c4bfca3a8
Richard Crowder <riccro@gmail.com>
2015-08-30 20:57:07 +0100

    Force c/p gcc tools

--- src/cmake/MinGW-CrossCompile-Windows.cmake
+++ src/cmake/MinGW-CrossCompile-Windows.cmake
@@ -18,16 +18,17 @@ SET(CMAKE_CROSSCOMPILING 1)
 set(COMPILER_PREFIX "x86_64-w64-mingw32")
 
 # which compilers to use for C and C++
-find_program(CMAKE_RC_COMPILER NAMES windres)
-find_program(CMAKE_C_COMPILER NAMES ${COMPILER_PREFIX}-gcc)
-find_program(CMAKE_CXX_COMPILER NAMES ${COMPILER_PREFIX}-g++)
-find_program(CMAKE_AR ${COMPILER_PREFIX}-gcc-ar)
-find_program(CMAKE_NM ${COMPILER_PREFIX}-gcc-nm)
-find_program(CMAKE_RANLIB ${COMPILER_PREFIX}-gcc-ranlib)
+set(CMAKE_RC_COMPILER windres)
+set(CMAKE_C_COMPILER gcc)
+set(CMAKE_CXX_COMPILER g++)
+set(CMAKE_AR ar)
+set(CMAKE_NM nm)
+set(CMAKE_RANLIB ranlib)
 
 # Target environment located
 #SET(USER_ROOT_PATH /home/travis/build/rcrowder/travis-ci-test)
 #SET(CMAKE_FIND_ROOT_PATH  /usr/${COMPILER_PREFIX} ${USER_ROOT_PATH})
+SET(CMAKE_FIND_ROOT_PATH C:\\mingw-w64\\bin)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
