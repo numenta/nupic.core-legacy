8436c485e31a08fe32b2231ba4150b90f76a5f11
Sagan Bolliger <sbolliger@numenta.com>
2016-01-18 13:34:56 -0800

    Code refactor as per review. Variable rename

--- src/nupic/algorithms/Connections.cpp
+++ src/nupic/algorithms/Connections.cpp
@@ -85,17 +85,8 @@ Synapse Connections::createSynapse(const Segment& segment,
   vector<SynapseData>& synapses = cells_[segment.cell.idx].segments[segment.idx].synapses;
   if (synapses.size() == maxSynapsesPerSegment_)
   {
-    vector<SynapseData>::iterator minPermSynIt = min_element(
-      synapses.begin(),
-      synapses.end(),
-      [](const SynapseData &a, const SynapseData &b) -> bool
-      {
-        return a.permanence < b.permanence;
-      });
-
-    SynapseIdx minSynapseIdx = distance(synapses.begin(), minPermSynIt);
-    Synapse minSynapse(minSynapseIdx, segment);
-
+    Synapse minSynapse;
+    minPermanenceSynapse(segment, minSynapse);
     destroySynapse(minSynapse);
   }
   Synapse synapse(synapses.size(), segment);
@@ -293,6 +284,31 @@ bool Connections::leastRecentlyUsedSegment(const Cell& cell,
   }
 
   return found;
+}
+
+bool Connections::minPermanenceSynapse(const Segment& segment,
+                                       Synapse& retSynapse) const
+{
+  const vector<SynapseData>& synapses =
+    cells_[segment.cell.idx].segments[segment.idx].synapses;
+
+  if (synapses.size() == 0)
+  {
+    return false;
+  }
+
+  vector<SynapseData>::const_iterator minSynapseIterator = min_element(
+    synapses.begin(),
+    synapses.end(),
+    [](const SynapseData &a, const SynapseData &b) -> bool
+    {
+      return a.permanence < b.permanence;
+    });
+
+  SynapseIdx minSynapseIdx = distance(synapses.begin(), minSynapseIterator);
+  retSynapse = Synapse(minSynapseIdx, segment);
+
+  return true;
 }
 
 Activity Connections::computeActivity(const vector<Cell>& input,
