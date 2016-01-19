e8fc1ca43001baaf3dfebe2312a7382e0364775a
Scott Purdy <spurdy@numenta.com>
2016-01-19 15:04:49 -0800

    Fix Linux build by removing -static link flag from shared library.

--- bindings/py/setup.py
+++ bindings/py/setup.py
@@ -213,7 +213,7 @@ if __name__ == "__main__":
     # This distribution contains platform-specific C++ libraries, but they are not
     # built with distutils. So we must create a dummy Extension object so when we
     # create a binary file it knows to make it platform-specific.
-    ext_modules=[Extension('nupic.dummy', sources = ['dummy.c'])], 
+    ext_modules=[Extension('nupic.dummy', sources = ['dummy.c'])],
     namespace_packages=["nupic"],
     install_requires=findRequirements(platform),
     packages=find_packages(),
