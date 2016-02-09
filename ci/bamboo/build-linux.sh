931d902b5795641fe5eebfbdc86643b0e157354b
Austin Marshall <amarshall@numenta.com>
2016-02-09 10:04:48 -0800

    Parameterized compiler

--- ci/bamboo/build-linux.sh
+++ ci/bamboo/build-linux.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 set -o errexit
 set -o xtrace
+
+# Environment defaults w/ defaults
+${USER:=docker}
+export USER
+
+${CC:="gcc"}
+export CC
+
+if [ "${CC}" = "clang" ]; then
+    ${CXX:=clang++}
+    COMPILER_PACKAGES="clang-3.4"
+else
+    ${CXX:=g++}
+    COMPILER_PACKAGES="${CC} ${CXX}"
+fi
+export CXX
+
+# Install OS dependencies, assuming stock ubuntu:latest
 apt-get update
 apt-get install -y \
     curl \
     wget \
     git-core \
-    gcc \
-    g++ \
+    ${COMPILER_PACKAGES} \
     cmake \
     python \
     python2.7 \
@@ -19,13 +36,14 @@ apt-get install -y \
 wget https://bootstrap.pypa.io/get-pip.py -O - | python
 pip install --upgrade setuptools
 pip install wheel
-export CC=gcc
-export CXX=g++
+
+# Install nupic.core dependencies
 pip install \
     --cache-dir /usr/local/src/nupic.core/pip-cache \
     --build /usr/local/src/nupic.core/pip-build \
     --no-clean \
     pycapnp==0.5.5 \
     -r bindings/py/requirements.txt
+
+# Build installable python packages
 python setup.py bdist bdist_dumb bdist_wheel sdist
-ls -laFh bindings/py/dist
