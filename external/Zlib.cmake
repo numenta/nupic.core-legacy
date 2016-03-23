4fc0f0b0858104e209192bd9c386de423a30cb2d
Vitaly Kruglikov <vkruglikov@numenta.com>
2016-03-23 14:03:58 -0700

    Integrating ZlibStaticLib build

---
+++ external/Zlib.cmake
@@ -0,0 +1,30 @@
+# -----------------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2016, Numenta, Inc.  Unless you have purchased from
+# Numenta, Inc. a separate commercial license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# -----------------------------------------------------------------------------
+
+
+get_filename_component(REPOSITORY_DIR ${PROJECT_SOURCE_DIR}/.. ABSOLUTE)
+
+
+ExternalProject_Add(
+    ZlibStaticLib
+    SOURCE_DIR ${REPOSITORY_DIR}/external/common/share/zlib/zlib-1.2.8
+    UPDATE_COMMAND ""
+)
