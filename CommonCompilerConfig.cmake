20c0f0430badec6c6dbca2c5ba369ade6b0631e6
Vitaly Kruglikov <vkruglikov@numenta.com>
2016-03-27 14:58:23 -0700

    Change internally-used vars in CommonCompilerConfig.cmake to lower case.

--- CommonCompilerConfig.cmake
+++ CommonCompilerConfig.cmake
@@ -21,6 +21,8 @@
 
 # Configures common compiler/linker/loader settings for internal and external
 # sources.
+#
+# NOTE SETTINGS THAT ARE SPECIFIC TO THIS OR THAT MODULE DO NOT BELONG HERE.
 
 # INPUTS:
 #
@@ -38,9 +40,11 @@
 #
 # COMMON_CXX_FLAGS: string of common C++ flags (COMMON_C_FLAGS + C++ flags)
 #
-# COMMON_LINK_FLAGS: string of linker flags
+# COMMON_LINK_FLAGS: string of linker flags for linking executables and shared
+#                    libraries (DLLs)
 #
-# CMAKE_LINKER: updated, if needed; see cmake documentation
+# CMAKE_LINKER: updated, if needed; use ld.gold if available. See cmake
+#               documentation
 
 if(NOT DEFINED PLATFORM)
     message(FATAL_ERROR "PLATFORM property not defined: PLATFORM=${PLATFORM}")
@@ -55,15 +59,15 @@ else()
   set(BITNESS 32)
 endif()
 
-set(STDLIB_CXX "")
+set(stdlib_cxx "")
 
 if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-  set(STDLIB "")
-  set(STDLIB_CXX "-stdlib=libc++")
+  set(stdlib_common "")
+  set(stdlib_cxx "-stdlib=libc++")
 elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-  set(STDLIB "")
+  set(stdlib_common "")
 elseif(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
-  set(STDLIB "")
+  set(stdlib_common "")
 endif()
 
 
@@ -105,8 +109,8 @@ if(EXIT_CODE EQUAL 0)
 endif()
 
 if (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
-   set(STDLIB "${STDLIB} -static-libgcc")
-   set(STDLIB_CXX "${STDLIB_CXX} -static-libstdc++")
+   set(stdlib_common "${stdlib_common} -static-libgcc")
+   set(stdlib_cxx "${stdlib_cxx} -static-libstdc++")
 endif()
 
 
@@ -116,14 +120,14 @@ endif()
 # for strange reasons, try compiling without them.
 #
 if(NOT ${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
-  set(OPTIMIZATION_FLAGS_CC "${OPTIMIZATION_FLAGS_CC} -mtune=generic -O2")
-  set(OPTIMIZATION_FLAGS_CC "-pipe ${OPTIMIZATION_FLAGS_CC}") #TODO use -Ofast instead of -O3
-  set(OPTIMIZATION_FLAGS_LT "-O2 ${OPTIMIZATION_FLAGS_LT}")
+  set(optimization_flags_cc "${optimization_flags_cc} -mtune=generic -O2")
+  set(optimization_flags_cc "-pipe ${optimization_flags_cc}") #TODO use -Ofast instead of -O3
+  set(optimization_flags_lt "-O2 ${optimization_flags_lt}")
 
   if(${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU" AND NOT MINGW)
-    set(OPTIMIZATION_FLAGS_CC "${OPTIMIZATION_FLAGS_CC} -fuse-linker-plugin -flto-report")
-    set(OPTIMIZATION_FLAGS_CC "${OPTIMIZATION_FLAGS_CC} -flto -fuse-ld=gold") #TODO fix LTO for clang
-    set(OPTIMIZATION_FLAGS_LT "${OPTIMIZATION_FLAGS_LT} -flto") #TODO LTO for clang too
+    set(optimization_flags_cc "${optimization_flags_cc} -fuse-linker-plugin -flto-report")
+    set(optimization_flags_cc "${optimization_flags_cc} -flto -fuse-ld=gold") #TODO fix LTO for clang
+    set(optimization_flags_lt "${optimization_flags_lt} -flto") #TODO LTO for clang too
   endif()
 endif()
 
@@ -135,7 +139,7 @@ set(COMMON_CXX_FLAGS "")
 
 if(${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
   # MS Visual C
-  set(SHARED_COMPILE_FLAGS "/TP /Zc:wchar_t /Gm- /fp:precise /errorReport:prompt /W1 /WX- /GR /Gd /GS /Oy- /EHs /analyze- /nologo")
+  set(shared_compile_flags "/TP /Zc:wchar_t /Gm- /fp:precise /errorReport:prompt /W1 /WX- /GR /Gd /GS /Oy- /EHs /analyze- /nologo")
   set(COMMON_LINK_FLAGS "/NOLOGO /SAFESEH:NO /NODEFAULTLIB:LIBCMT")
   if("${BITNESS}" STREQUAL "32")
     set(COMMON_LINK_FLAGS "${COMMON_LINK_FLAGS} /MACHINE:X86")
@@ -147,22 +151,22 @@ else()
   # LLVM Clang / Gnu GCC
   set(COMMON_CXX_FLAGS "${COMMON_CXX_FLAGS} -std=c++11")
 
-  set(SHARED_COMPILE_FLAGS "-m${BITNESS} ${STDLIB} -Wextra -Wreturn-type -Wunused -Wno-unused-variable -Wno-unused-parameter -Wno-missing-field-initializers")
+  set(shared_compile_flags "-m${BITNESS} ${stdlib_common} -Wextra -Wreturn-type -Wunused -Wno-unused-variable -Wno-unused-parameter -Wno-missing-field-initializers")
   if(NOT ${CMAKE_SYSTEM_NAME} MATCHES "Windows")
-    set(SHARED_COMPILE_FLAGS "-Wall -fPIC ${SHARED_COMPILE_FLAGS} -Wno-deprecated-register")
+    set(shared_compile_flags "-Wall -fPIC ${shared_compile_flags} -Wno-deprecated-register")
   endif()
-  set(COMMON_LINK_FLAGS "-m${BITNESS} ${STDLIB} ${STDLIB_CXX}")
+  set(COMMON_LINK_FLAGS "-m${BITNESS} ${stdlib_common} ${stdlib_cxx}")
 endif()
 
 if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
-  set(SHARED_COMPILE_FLAGS "${SHARED_COMPILE_FLAGS} -g")
+  set(shared_compile_flags "${shared_compile_flags} -g")
   set(COMMON_LINK_FLAGS "${COMMON_LINK_FLAGS} -O0")
   if(${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU" OR MINGW)
-    set(SHARED_COMPILE_FLAGS "${SHARED_COMPILE_FLAGS} -Og")
+    set(shared_compile_flags "${shared_compile_flags} -Og")
   endif()
 else()
-  set(SHARED_COMPILE_FLAGS "${SHARED_COMPILE_FLAGS} ${OPTIMIZATION_FLAGS_CC}")
-  set(COMMON_LINK_FLAGS "${COMMON_LINK_FLAGS} ${OPTIMIZATION_FLAGS_LT}")
+  set(shared_compile_flags "${shared_compile_flags} ${optimization_flags_cc}")
+  set(COMMON_LINK_FLAGS "${COMMON_LINK_FLAGS} ${optimization_flags_lt}")
 endif()
 
 # Set compiler-specific options.
@@ -172,9 +176,9 @@ elseif(${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
   set(COMMON_LINK_FLAGS "${COMMON_LINK_FLAGS} -Wl,-undefined,error")
 endif()
 
-set(COMMON_C_FLAGS "${SHARED_COMPILE_FLAGS}")
+set(COMMON_C_FLAGS "${shared_compile_flags}")
 
-set(COMMON_CXX_FLAGS "${COMMON_CXX_FLAGS} ${SHARED_COMPILE_FLAGS} ${STDLIB_CXX}")
+set(COMMON_CXX_FLAGS "${COMMON_CXX_FLAGS} ${shared_compile_flags} ${stdlib_cxx}")
 
 # Provide a string variant of COMMON_COMPILER_DEFINITIONS list
 string (REPLACE ";" " " COMMON_COMPILER_DEFINITIONS_STR "${COMMON_COMPILER_DEFINITIONS}")
