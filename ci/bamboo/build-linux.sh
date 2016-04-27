c227adbaffd87999339753f400b2b1a295b51cc0
Scott Purdy <spurdy@numenta.com>
2016-04-27 11:16:05 -0700

    Move common environment setup for Bamboo into separate script.

--- ci/bamboo/build-linux.sh
+++ ci/bamboo/build-linux.sh
@@ -2,45 +2,10 @@
 set -o errexit
 set -o xtrace
 
-# Environment defaults
-if [ -z "${USER}" ]; then
-    USER="docker"
-fi
-export USER
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Setup compiler
-if [ -z "${CC}" ]; then
-    CC="gcc"
-fi
-export CC
-
-if [ "${CC}" = "clang" ]; then
-    if [ -z "${CXX}" ]; then
-        CXX="clang++"
-    fi
-    COMPILER_PACKAGES="clang-3.4" # Ubuntu-specific apt package name
-else
-    if [ -z "${CXX}" ]; then
-        CXX="g++"
-    fi
-    COMPILER_PACKAGES="${CC} ${CXX}" # Ubuntu-specific apt package names
-fi
-export CXX
-
-# Install OS dependencies, assuming stock ubuntu:latest
-apt-get update
-apt-get install -y \
-    curl \
-    wget \
-    git-core \
-    ${COMPILER_PACKAGES} \
-    cmake \
-    python \
-    python2.7 \
-    python2.7-dev
-wget https://bootstrap.pypa.io/get-pip.py -O - | python
-pip install --upgrade --ignore-installed setuptools
-pip install wheel
+# Run the common setup
+${DIR}/setup-dependencies.sh
 
 # Install nupic.core dependencies
 pip install \
