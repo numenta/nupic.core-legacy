85ad8f4c459420d74cb29a18b2897fc34ec44834
Vitaly Kruglikov <vkruglikov@numenta.com>
2016-08-16 18:38:23 -0700

    DEVOPS-134 Restructure manylinux wheel build scripts to conform with requirements of the "run in docker" Bamboo task
    DEVOPS-134 Factored out generic nupic.bindings build/test logic into ci/build-and-test-nupic-bindings.sh
    
    DEVOPS-134 Created ci/bamboo/build-manylinux-release.sh and build-manylinux-debug.sh
    based on shared generic ci/build-and-test-nupic-bindings.sh
    
    DEVOPS-134 Modified ci/bamboo/build-linux-release.sh to make use of the shared generic
    ci/build_nupic_bindings.sh. This is a temporary change just to test out the
    ci/build_nupic_bindings.sh. Once ci/bamboo/build-manylinux-release.sh is integrated
    into nupic.core build plan, the legacy build-linux-release.sh and build-linux-debug.sh
    will be deleted.
    
    DEVOPS-134 Display message and fail build if src/CMakeLists.txt is unable to get pycapnp's capnproto headers location.

--- ci/bamboo/build-linux-release.sh
+++ ci/bamboo/build-linux-release.sh
@@ -8,20 +8,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ${DIR}/setup-dependencies-linux.sh
 
 # Install nupic.core dependencies
-pip install \
-    --cache-dir /usr/local/src/nupic.core/pip-cache \
-    --build /usr/local/src/nupic.core/pip-build \
-    --no-clean \
-    pycapnp==0.5.8 \
-    -r bindings/py/requirements.txt
-
-# Build and install nupic.core
-mkdir -p build/scripts
-cmake -DCMAKE_INSTALL_PREFIX=`pwd`/build/release -DPY_EXTENSIONS_DIR=`pwd`/bindings/py/nupic/bindings .
-make install
-./build/release/bin/cpp_region_test
-./build/release/bin/unit_tests
-
-# Build installable python packages
-python setup.py bdist_wheel
-py.test --junitxml bindings/py/linux-clang-debug-results-${bamboo_buildResultKey}.xml --cov nupic.bindings bindings/py/tests
+BUILD_TYPE="Release" \
+RESULT_KEY="${bamboo_buildResultKey}" \
+  ${DIR}/../build-and-test-nupic-bindings.sh "$@"
