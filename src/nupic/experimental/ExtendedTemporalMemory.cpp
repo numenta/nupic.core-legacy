ac4910859d23ef91cd921a6e633b236e73d1e883
Marcus Lewis <mrcslws@gmail.com>
2016-08-23 00:54:11 -0700

    Check that minThreshold <= activationThreshold

--- src/nupic/experimental/ExtendedTemporalMemory.cpp
+++ src/nupic/experimental/ExtendedTemporalMemory.cpp
@@ -127,6 +127,7 @@ void ExtendedTemporalMemory::initialize(
   NTA_CHECK(connectedPermanence >= 0.0 && connectedPermanence <= 1.0);
   NTA_CHECK(permanenceIncrement >= 0.0 && permanenceIncrement <= 1.0);
   NTA_CHECK(permanenceDecrement >= 0.0 && permanenceDecrement <= 1.0);
+  NTA_CHECK(minThreshold <= activationThreshold);
 
   // Save member variables
 
