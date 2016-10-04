7a2f3048d427f19bbcfdc9428f1e43c839f3892c
Matthew Taylor <matt@numenta.org>
2016-10-04 14:34:09 -0700

    add --user only on linux builds

--- ci/travis/before_install-linux.sh
+++ ci/travis/before_install-linux.sh
@@ -37,6 +37,8 @@ if [ $CC = 'clang' ]; then
     export CXX='clang++'
 fi
 
+export PIP_USER="--user"
+
 export PATH=$HOME/.local/bin:$PATH
 export PYTHONPATH=$HOME/.local/lib/python2.7/site-packages:$PYTHONPATH
 
