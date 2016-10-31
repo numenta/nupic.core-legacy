ce49f678513af09d25fc4a883d8d4295aaacfb16
Matthew Taylor <matt@numenta.org>
2016-10-31 13:26:13 -0700

    Updated to numpy 1.11.2 everywhere.

--- ci/appveyor/install_python_pip.ps1
+++ ci/appveyor/install_python_pip.ps1
@@ -97,8 +97,8 @@ function main () {
     Write-Host "pip install " twine
     & $pip_path install twine
 
-    Write-Host "pip install " numpy==1.9.2
-    & $pip_path install -i https://pypi.anaconda.org/mingwpy/simple numpy==1.9.2
+    Write-Host "pip install " numpy==1.11.2
+    & $pip_path install -i https://pypi.anaconda.org/mingwpy/simple numpy==1.11.2
 }
 
 main
