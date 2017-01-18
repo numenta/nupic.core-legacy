855e772adbf7ee0454f389bbe0ca5f10c89e4fa4
Austin Marshall <amarshall@numenta.com>
2017-01-17 16:59:18 -0800

    Fixup import favoring native unittest

--- bindings/py/tests/network_test.py
+++ bindings/py/tests/network_test.py
@@ -19,7 +19,7 @@
 # http://numenta.org/licenses/
 # ----------------------------------------------------------------------
 
-import unittest2 as unittest
+import unittest
 
 from nupic import engine
 
