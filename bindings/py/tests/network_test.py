fe8ae253f9b84d36f76525a384856ff0bc965f51
Austin Marshall <amarshall@numenta.com>
2017-01-17 19:05:23 -0800

    Fixup comment positioning

--- bindings/py/tests/network_test.py
+++ bindings/py/tests/network_test.py
@@ -35,10 +35,9 @@ class NetworkTest(unittest.TestCase):
     network.addRegion("region1", "TestNode", "")
     network.addRegion("region2", "TestNode", "")
 
+    # Set dimensions on first region
     region1 = network.getRegions().getByName("region1")
     region1.setDimensions(engine.Dimensions([1, 1]))
-
-    # Set dimensions on first region
 
     # Link region1 and region2
     network.link("region1", "region2", "UniformLink", "")
