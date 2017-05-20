e0dee970864b87a1a3c84625356259b430e03c9f
Marek Otahal <markotahal@gmail.com>
2017-05-20 13:30:33 +0200

    SlidingWindow: merge implementation and definitions
    as it's in templated header anyway

--- src/nupic/utils/SlidingWindow.hpp
+++ src/nupic/utils/SlidingWindow.hpp
@@ -26,6 +26,7 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <cmath>
 
 #include <nupic/types/Types.hpp>
 #include <nupic/utils/Log.hpp>
@@ -34,63 +35,132 @@
 namespace nupic {
   namespace util {
 
-    template<class T> 
-    class SlidingWindow {
-      public:
-        SlidingWindow(UInt maxCapacity);
-        template<class IteratorT> 
-        SlidingWindow(UInt maxCapacity, IteratorT initialData_begin, IteratorT initialData_end): 
-          SlidingWindow(maxCapacity) {
-  // Assert that It obeys the STL forward iterator concept
-  ASSERT_INPUT_ITERATOR(IteratorT);
-  for(IteratorT it = initialData_begin; it != initialData_end; ++it) {
-    append(*it);
-  }
-  idxNext_ = buffer_.size() % maxCapacity;
+template<class T> 
+class SlidingWindow {
+  public:
+    SlidingWindow(UInt maxCapacity) : maxCapacity(maxCapacity) {
+      buffer_.reserve(maxCapacity);
+      idxNext_ = 0;
+  } 
+
+
+    template<class IteratorT> 
+    SlidingWindow(UInt maxCapacity, IteratorT initialData_begin, IteratorT initialData_end): 
+      SlidingWindow(maxCapacity) {
+      // Assert that It obeys the STL forward iterator concept
+      ASSERT_INPUT_ITERATOR(IteratorT);
+      for(IteratorT it = initialData_begin; it != initialData_end; ++it) {
+        append(*it);
+      }
+      idxNext_ = buffer_.size() % maxCapacity;
+    }
+
+    const UInt maxCapacity;
+
+    size_t size() const {
+      NTA_ASSERT(buffer_.size() <= maxCapacity);
+      return buffer_.size();
+    }
+
+
+    /** append new value to the end of the buffer and handle the 
+       "overflows"-may pop the oldest element if full. 
+      */
+    void append(T newValue) {
+      if(size() < maxCapacity) {
+        buffer_.push_back(newValue);
+      } else {
+        buffer_[idxNext_] = newValue;
+      }
+      //the assignment must be out of the [] above, so not [idxNext_++%maxCap],
+      // because we want to store the value %maxCap, not only ++
+      idxNext_ = (idxNext_ +1 ) %maxCapacity;
+    }
+
+
+      /** like append, but return the dropped value if it was dropped.
+        :param T newValue - new value to append to the sliding window
+        :param T& - a return pass-by-value with the removed element,
+          if this function returns false, this value will remain unchanged.
+        :return bool if some value has been dropped (and updated as 
+          droppedValue) 
+      */
+      bool append(T newValue, T& droppedValue) {
+        //only in this case we drop oldest; this happens always after
+        //first maxCap steps ; must be checked before append()
+        bool isFull = (buffer_.size()==maxCapacity);
+        if(isFull) {
+          droppedValue = buffer_[idxNext_];
         }
-        const UInt maxCapacity;
-        size_t size() const;
-        /** append new value to the end of the buffer and handle the 
-           "overflows"-may pop the first element if full. 
-        */
-        void append(T newValue);
+        append(newValue);
+        return isFull;
+      }
 
-        /** like append, but return the dropped value if it was dropped.
-          :param T newValue - new value to append to the sliding window
-          :param T& - a return pass-by-value with the removed element,
-             if this function returns false, this value will remain unchanged.
-          :return bool if some value has been dropped (and updated as 
-       droppedValue) 
-        */
-        bool append(T newValue, T& droppedValue);
 
-        /**
-        * :return unordered content (data ) of this sl. window; 
-             call getLinearizedData() if you need them oredered from 
-             oldest->newest
-        * This direct access method is fast.
-        */
-        const std::vector<T>& getData() const;
+      /**
+        :return unordered content (data ) of this sl. window; 
+          call getLinearizedData() if you need them oredered from 
+          oldest->newest
+        This direct access method is fast.
+      */
+      const std::vector<T>& getData() const {
+        return buffer_;
+      }
 
-        /** linearize method for the internal buffer; this is slower than 
-            the pure getData() but ensures that the data are ordered (oldest at
-             the beginning, newest at the end of the vector
-         * This handles case of |5,6;1,2,3,4| => |1,2,3,4,5,6|
-         * :return new linearized vector
-        */
-        std::vector<T> getLinearizedData() const;
 
-        bool operator==(const SlidingWindow& r2) const;
-        bool operator!=(const SlidingWindow& r2) const;
-        /** operator[] provides fast access to the elements indexed relatively
-          to the oldest element. So slidingWindow[0] returns oldest element, 
-          slidingWindow[size()] returns the newest. 
-        :param UInt index - index/offset from the oldest element, values 0..size()
-        :return T - i-th oldest value in the buffer
-        :throws 0<=index<=size()
-        */ 
-        T& operator[](UInt index);
-        const T& operator[](UInt index) const;
+      /** linearize method for the internal buffer; this is slower than 
+        the pure getData() but ensures that the data are ordered (oldest at
+        the beginning, newest at the end of the vector
+        This handles case of |5,6;1,2,3,4| => |1,2,3,4,5,6|
+        :return new linearized vector
+      */
+      std::vector<T> getLinearizedData() const {
+        std::vector<T> lin;
+        lin.reserve(buffer_.size());
+
+        //insert the "older" part at the beginning
+        lin.insert(std::begin(lin), std::begin(buffer_) + idxNext_, std::end(buffer_));
+        //append the "newer" part to the end of the constructed vect
+        lin.insert(std::end(lin), std::begin(buffer_), std::begin(buffer_) + idxNext_);
+        return lin;
+      }
+
+
+      bool operator==(const SlidingWindow& r2) const {
+        return ((this->size() == r2.size()) && (this->maxCapacity == r2.maxCapacity) &&
+          (this->getData()== r2.getData()) && getLinearizedData() == r2.getLinearizedData());
+        //FIXME review the ==, on my machine it randomly passes/fails the test!
+      }
+
+
+      bool operator!=(const SlidingWindow& r2) const {
+        return !operator==(r2);
+      }
+
+
+      /** operator[] provides fast access to the elements indexed relatively
+        to the oldest element. So slidingWindow[0] returns oldest element, 
+        slidingWindow[size()] returns the newest. 
+      :param UInt index - index/offset from the oldest element, values 0..size()
+      :return T - i-th oldest value in the buffer
+      :throws 0<=index<=size()
+      */ 
+      T& operator[](UInt index) {
+        NTA_ASSERT(index <= size());
+        NTA_ASSERT(size() > 0);
+        //get last updated position, "current"+index(offset)
+        //avoid calling getLinearizeData() as it involves copy()
+        if (size() == maxCapacity) {
+          return &buffer_[(idxNext_ + index) % maxCapacity];
+        } else {
+          return &buffer_[index];
+        }
+      }
+
+
+      const T& operator[](UInt index) const {
+        return this->operator[](index); //call the overloaded operator[] above
+      }
 
 
       private:
@@ -98,105 +168,4 @@ namespace nupic {
         UInt idxNext_;
 }; 
 }} //end ns
-
-/// IMPLEMENTATION
-#include <cmath>
-
-using nupic::util::SlidingWindow;
-
-template<class T>
-SlidingWindow<T>::SlidingWindow(nupic::UInt maxCapacity) :
-  maxCapacity(maxCapacity) {
-  NTA_CHECK(maxCapacity > 0);
-  buffer_.reserve(maxCapacity);
-  idxNext_ = 0;
-}
-
-
-template<class T>
-size_t SlidingWindow<T>::size() const {
-  NTA_ASSERT(buffer_.size() <= maxCapacity);
-  return buffer_.size();
-}
-
-
-template<class T>
-void SlidingWindow<T>::append(T newValue) {
-  if(size() < maxCapacity) {
-    buffer_.push_back(newValue);
-  } else {
-  buffer_[idxNext_] = newValue;
-  }
-  //the assignment must be out of the [] above, so not [idxNext_++%maxCap],
-  // because we want to store the value %maxCap, not only ++
-  idxNext_ = (idxNext_ +1 ) %maxCapacity;
-}
-
-
-template<class T>
-bool SlidingWindow<T>::append(T newValue, T& droppedValue) {
-  //only in this case we drop oldest; this happens always after 
-  //first maxCap steps ; must be checked before append()
-  bool isFull = (buffer_.size()==maxCapacity); 
-  if(isFull) { 
-    droppedValue = buffer_[idxNext_]; 
-  }
-  append(newValue);
-  return isFull;
-}
-
-
-template<class T>
-const std::vector<T>& SlidingWindow<T>::getData() const {
-  return buffer_; 
-}
-
-
-template<class T>
-std::vector<T> SlidingWindow<T>::getLinearizedData() const {
-  std::vector<T> lin;
-  lin.reserve(buffer_.size());
-
-  //insert the "older" part at the beginning
-  lin.insert(std::begin(lin), std::begin(buffer_) + idxNext_, std::end(buffer_));
-  //append the "newer" part to the end of the constructed vect
-  lin.insert(std::end(lin), std::begin(buffer_), std::begin(buffer_) + idxNext_);
- return lin;
-}
-
-
-template<class T>
-bool SlidingWindow<T>::operator==(const SlidingWindow& r2) const {
-  return ((this->size() == r2.size()) && (this->maxCapacity == r2.maxCapacity) && 
-    (this->getData()== r2.getData()) && getLinearizedData() == r2.getLinearizedData()); 
-  //FIXME review the ==, on my machine it randomly passes/fails the test!
-}
-
-
-template<class T>
-bool SlidingWindow<T>::operator!=(const SlidingWindow& r2) const {
-  return !operator==(r2);
-}
-
-
-template<class T> 
-T& SlidingWindow<T>::operator[](nupic::UInt index) {
-  NTA_ASSERT(index <= size());
-  NTA_ASSERT(size() > 0);
-  //get last updated position, "current"+index(offset)
-  //avoid calling getLinearizeData() as it involves copy()
-  if (size() == maxCapacity) {
-    return &buffer_[(idxNext_ + index) % maxCapacity];
-  }
-  else {
-    return &buffer_[index];
-  } 
-}
-
-
-template<class T>
-const T& SlidingWindow<T>::operator[](nupic::UInt index) const {
-  return this->operator[](index); //call the overloaded operator[] above 
-}
-
 #endif //header
