bb56704f71df90d0f7682e8db0563a83c6ecfdd8
Scott Purdy <spurdy@numenta.com>
2017-06-07 16:44:01 -0700

    Code review updates

--- src/nupic/algorithms/ClassifierResult.cpp
+++ src/nupic/algorithms/ClassifierResult.cpp
@@ -49,8 +49,7 @@ namespace nupic
       vector<Real64>* ClassifierResult::createVector(Int step, UInt size,
                                                 Real64 value)
       {
-        map<Int, vector<Real64>*>::const_iterator it = result_.find(step);
-        NTA_CHECK(it == result_.end())
+        NTA_CHECK(result_.count(step) == 0)
           << "The ClassifierResult cannot be reused!";
         vector<Real64>* v = new vector<Real64>(size, value);
         result_.insert(pair<Int, vector<Real64>*>(step, v));
