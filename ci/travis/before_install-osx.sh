428f0075fb03ea4271f139ae695eed84654af2af
Marek Otahal <markotahal@gmail.com>
2018-01-20 00:26:25 +0100

    CI Travis OSX: bump pycapnp to requirements.txt
    version

--- ci/travis/before_install-osx.sh
+++ ci/travis/before_install-osx.sh
@@ -44,4 +44,4 @@ python get-pip.py --user pip setuptools wheel
 echo "Installing Python dependencies"
 pip install --use-wheel --user -r bindings/py/requirements.txt --quiet || exit
 
-pip install pycapnp==0.5.8 --user || exit
+pip install pycapnp==0.5.12 --user || exit
