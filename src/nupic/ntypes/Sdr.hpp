f4dc2b73db948ff72161d372198c751dd0b4a12c
David McDougall <ctrl-z-9000-times@users.noreply.github.com>
2018-11-16 08:42:34 -0500

    Add files via upload

--- src/nupic/ntypes/Sdr.hpp
+++ src/nupic/ntypes/Sdr.hpp
@@ -37,22 +37,34 @@ namespace nupic {
  * ### Description
  * This class manages the specification and momentary value of a Sparse
  * Distributed Representation (SDR).  An SDR is a group of boolean values which
- * represent the state of a group of neurons or their associated processes. This
- * class automatically converts between the commonly used SDR data formats:
+ * represent the state of a group of neurons or their associated processes. 
+ *
+ * This class automatically converts between the commonly used SDR data formats:
  * which are dense, index, and flat-index.  Converted values are cached by this
  * class, so getting a value in one format many times incurs no extra
- * performance cost.  This class uses C-order throughout, meaning that when
- * iterating through the SDR, the last/right-most index changes fastest.
+ * performance cost.  Assigning to the SDR via a setter method will clear these
+ * cached values and cause them to be recomputed as needed.
  *
- * Dense Format: A contiguous array of boolean values, representing all of the
- * bits in the SDR.
+ *    Dense Format: A contiguous array of boolean values, representing all of
+ *    the bits in the SDR.  This format allows random-access queries of the SDRs
+ *    values.
  *
- * Index Format: A list of lists of the indices of the true bits in the SDR. The
- * outter list contains an entry for each dimension in the SDR.  The inner lists
- * contain the coordinates of each true bit.  The inner lists run in parallel.
+ *    Index Format: A list of lists of the indices of the true bits in the SDR.
+ *    The outter list contains an entry for each dimension in the SDR.  The
+ *    inner lists contain the coordinates of each true bit.  The inner lists run
+ *    in parallel. This format is useful because it contains the location of
+ *    each true bit inside of the SDR's dimensional space.
  *
- * Flat Index Format: A list of the indices of the true bits, indexed into the
- * the flattened SDR.
+ *    Flat Index Format: A list of the indices of the true bits, indexed into
+ *    the the flattened SDR.  This format allows for quickly accessing all of
+ *    the true bits in the SDR.
+ *
+ * Flat Formats: This class uses C-order throughout, meaning that when iterating
+ * through the SDR, the last/right-most index changes fastest.
+ *
+ * Boolean Data Type: Although the documentation for this class may refer to the
+ * values in an SDR as bits they are in fact stored as bytes (of type signed
+ * character).
  *
  * Example usage:
  *
@@ -60,8 +72,8 @@ namespace nupic {
  *    SparseDistributedRepresentation X({100, 100});
  *
  *    // Assign data in any format.
- *    X.setFlatIndex( myFlatIndex ); // Assigns myFlatIndex to SDR's value.
- *    X.setDense( myDenseData );     // myDenseData overwrites myFlatIndex.
+ *    X.setFlatIndex( mySparseData ); // Assigns mySparseData to SDR's value.
+ *    X.setDense( myDenseData );      // myDenseData overwrites mySparseData.
  *
  *    // Access data in different formats.
  *    indices      = X.getFlatIndex(); // Calculated from myDenseData.
@@ -72,32 +84,37 @@ class SparseDistributedRepresentation : public Serializable
 {
 public:
     /**
-     *
+     * Create an uninitialized SDR.  The only way to initialize SDRs created by
+     * this method is by calling SDR.load().  Until SDR.load() is called this
+     * SDR is in an unusable state.
      */
     SparseDistributedRepresentation();
 
     /**
+     * Create an SDR object.  Initially this SDR has no value set.
      *
-     * @param
+     * @param dimensions A list of dimension sizes, defining the shape of the
+     * SDR.
      */
     SparseDistributedRepresentation(const vector<UInt> dimensions);
 
     /**
      * Initialize this SDR as a copy of the given SDR.  This is a deep copy
-     * meaning that all of the data in the given SDR is fully copied.
+     * meaning that all of the data in the given SDR is fully copied, and
+     * modifying either SDR will not affect the other SDR.
      *
      * @param value An SDR to replicate.
      */
     SparseDistributedRepresentation(const SparseDistributedRepresentation value);
 
     /**
-     * @returns A vector of dimensions for the SDR.
+     * @returns A list of dimensions of the SDR.
      */
     const vector<UInt> getDimensions() const
         {return dimensions;};
 
     /**
-     * @returns The total number of bits in the SDR.
+     * @returns The total number of boolean values in the SDR.
      */
     const UInt getSize() const
         {return size;};
@@ -109,70 +126,116 @@ public:
     void clear();
 
     /**
-     * Set all of the bits in the SDR to 0's.
+     * Set all of the values in the SDR to false.  This method overwrites the
+     * SDRs current value.
      */
     void zero();
 
     /**
-     * @param value A dense boolean vector to assign to the SDR.
+     * Assigns a new value to the SDR, overwritting the current value.
+     *
+     * @param value A dense array in a vector<char> to assign to the SDR.
      */
-    void setDense( const vector<bool> value );
+    void setDense( const vector<Byte> value );
 
     /**
-     * @param value A dense C-style array of UInt's to assign to the SDR.
+     * Assigns a new value to the SDR, overwritting the current value.
+     *
+     * @param value A dense C-style array of UInt's to assign to the SDR.  The
+     * given value must have as many elements as this SDR, as given by 
+     * SDR.getSize().
      */
     void setDense( const UInt *value );
 
     /**
-     * @param value A dense boolean Array to assign to the SDR.
+     * Assigns a new value to the SDR, overwritting the current value.
+     *
+     * @param value A dense byte Array to assign to the SDR.
      */
     void setDense( const ArrayBase *value );
 
     /**
-     * Assign a vector of sparse indices to the SDR.
+     * Assigns a vector of sparse indices to the SDR.  These indicies are into
+     * the flattened SDR space.  This overwrites the SDR's current value.
      *
      * @param value A vector of indices to assign to the SDR.
      */
     void setFlatIndex( const vector<UInt> value );
 
     /**
+     * Assigns an array of sparse indices to the SDR.  These indicies are into
+     * the flattened SDR space.  This overwrites the SDR's current value.
+     *
      * @param value A C-style array of indices to assign to the SDR.
      * @param num_values The number of elements in the 'value' array.
      */
     void setFlatIndex( const UInt *value, const UInt num_values );
 
     /**
+     * Assign a list of indices to the SDR, overwritting the SDRs current value.
+     * These indices are into the SDR space with dimensions.  The outter list
+     * is indexed using an index into the dimensions list, see
+     * SDR.getDimensions().  The inner lists are indexed in parallel, they
+     * contain the coordinates of the true values in the SDR.
      *
+     * @param value A list of lists containing the coordinates of the true
+     * values to assign to the SDR.
      */
     void setIndex( const vector<vector<UInt>> value );
 
     /**
+     * Assigns a deep copy of the given SDR to this SDR.  This overwrites the
+     * current value of this SDR.
+     *
      * @param value An SDR to copy the value of.
      */
     void assign( const SparseDistributedRepresentation value );
 
     /**
-     * @returns TODO
+     * Gets the current value of the SDR.  The result of this method call is
+     * saved inside of this SDR until the SDRs value changes.  If the value
+     * is unset this raises an exception.
+     *
+     * @returns A list of all values in the SDR.
      */
-    const vector<bool> getDense();
+    const vector<Byte> getDense();
 
     /**
-     * @returns TODO
+     * Gets the current value of the SDR.  The result of this method call is
+     * saved inside of this SDR until the SDRs value changes.  If the value is
+     * unset this raises an exception.
+     *
+     * @returns A list of the indices of the true values in the flattened SDR.
      */
     const vector<UInt> getFlatIndex();
 
     /**
-     * @returns TODO
+     * Gets the current value of the SDR.  The result of this method call is
+     * saved inside of this SDR until the SDRs value changes.  If the value is
+     * unset this raises an exception.
+     *
+     * @returns A list of lists of the coordinates of the true values in the
+     * SDR.  The outter list has an entry for each dimension in the SDR.  The
+     * inner lists have an entry for each true value in the SDR.
      */
     const vector<vector<UInt>> getIndex();
 
     /**
-     * @returns TODO
+     * Makes a deep copy of the SDR.  This SDR and the returned SDR have no
+     * shared data and they can be modified without affecting each other.
+     *
+     * @returns An SDR which is identical to this SDR.
      */
     const SparseDistributedRepresentation copy() const;
 
     /**
-     * @returns The fraction of bits in the SDR which are 1's.
+     * Calculates the sparsity of the SDR, which is the fraction of bits which
+     * are true out of the total number of bits in the SDR.
+     * I.E.  sparsity = SDR.getFlatIndex.size() / SDR.getSize()
+     *
+     * If the SDRs value is unset this raises an exception.
+     *
+     * @returns The fraction of values in the SDR which are true.
      */
     const Real getSparsity();
 
@@ -184,8 +247,8 @@ public:
     void save(std::ostream &stream) const override;
 
     /**
-     * Load (deserialize) and initialize the SDR from the
-     * specified input stream.
+     * Load (deserialize) and initialize the SDR from the specified input
+     * stream.
      *
      * @param stream A input valid istream, such as an open file.
      */
@@ -195,7 +258,7 @@ private:
     vector<UInt> dimensions;
     size_t       size;
 
-    vector<bool>         dense;
+    vector<Byte>         dense;
     vector<UInt>         flatIndex;
     vector<vector<UInt>> index;
 
