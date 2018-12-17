57981ce7ff2cdd83e5a72053c77fa74298c2d743
Marek Otahal <markotahal@gmail.com>
2018-12-17 15:27:24 +0100

    Revert "Revert "remove SparseTensor, Domain, Index""
    This reverts commit 3e26947653fdc59c2a156b67619d8c7abace1baf.

--- bindings/py/src/bindings/math/math_module.cpp
+++ bindings/py/src/bindings/math/math_module.cpp
@@ -41,21 +41,12 @@ namespace py = pybind11;
 namespace nupic_ext
 {
     void init_array_algo(py::module&);
-    void init_Domain(py::module&);
-    void init_Gaussian_2D(py::module&);
     void init_Math_Functions(py::module&);
-    void init_NearestNeighbor(py::module&);
     void init_Random(py::module&);
     void init_reals(py::module&);
     void init_Set(py::module&);
     void init_SM_01_32_32(py::module&);
     void init_SM32(py::module&);
-    void init_SMC(py::module&);
-    void init_SegmentSparseMatrix(py::module&);
-    void init_SparseTensor(py::module&);
-    void init_TensorIndex(py::module&);
-    void init_SparseMatrixAlgorithms(py::module&);
-    void init_SparseRLEMatrix(py::module&);
 } // namespace nupic_ext
 
 using namespace nupic_ext; 
@@ -64,15 +55,10 @@ PYBIND11_MODULE(math, m) {
     m.doc() = "nupic.bindings.math plugin"; // optional module docstring
 
     init_array_algo(m);
-    init_Domain(m);
     init_SM32(m); // has to be defined before NearestNeighbor
     init_Math_Functions(m);
     init_Random(m);
     init_reals(m);
-    init_SegmentSparseMatrix(m);
     init_Set(m);
     init_SM_01_32_32(m);
-    init_SMC(m);
-    init_SparseTensor(m);
-    init_TensorIndex(m);
 }
