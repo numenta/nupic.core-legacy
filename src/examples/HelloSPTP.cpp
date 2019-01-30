546675aff32c130cc37cde21910350f2b313623d
Marek Otahal <markotahal@gmail.com>
2019-01-30 15:17:26 +0100

    Hotgym: do not run getSpeed() for valgrind
    "hack" with EPOCHS > 1000

--- src/examples/HelloSPTP.cpp
+++ src/examples/HelloSPTP.cpp
@@ -136,15 +136,15 @@ void run(UInt EPOCHS = 5000) {
 
       const size_t timeTotal = (size_t)floor(tAll.getElapsed());
       cout << "Total elapsed time = " << timeTotal << " seconds" << endl;
-      #ifdef NDEBUG
-	    #ifdef _MSC_VER
+      if(EPOCHS >= 100) { //show only relevant values, ie don't run in valgrind (ndebug, epochs=5) run
+#ifdef _MSC_VER
           const size_t CI_avg_time = (size_t)floor(14*Timer::getSpeed()); //sec
-		#else
+#else
           const size_t CI_avg_time = (size_t)floor(7*Timer::getSpeed()); //sec
-		#endif
+#endif
         NTA_CHECK(timeTotal <= CI_avg_time) << //we'll see how stable the time result in CI is, if usable
           "HelloSPTP test slower than expected! (" << timeTotal << ",should be "<< CI_avg_time;
-      #endif
+      }
     }
   } //end for
 
