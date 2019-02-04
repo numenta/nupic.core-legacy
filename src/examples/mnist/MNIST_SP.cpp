f973f2448bd903a4971e4859d434fd7460f96479
ctrl-z-9000-times <dam1784@rit.edu>
2019-02-03 22:14:48 -0500

    Pull MNIST experiment

--- src/examples/mnist/MNIST_SP.cpp
+++ src/examples/mnist/MNIST_SP.cpp
@@ -17,8 +17,10 @@
 
 /**
  * Solving the MNIST dataset with Spatial Pooler.
- * TODO: DESCRIPTION
- * TODO: RUN INSTRUCTIONS
+ *
+ * This consists of a simple black & white image encoder, a spatial pool, and an
+ * SDR classifier.  The task is to recognise images of hand written numbers 0-9.
+ * This should score at least 95%.
  */
 
 #include <algorithm>
@@ -38,25 +40,6 @@ using namespace nupic;
 using nupic::algorithms::spatial_pooler::SpatialPooler;
 using nupic::algorithms::sdr_classifier::SDRClassifier;
 using nupic::algorithms::cla_classifier::ClassifierResult;
-
-
-#include <stdio.h>
-#include <execinfo.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <unistd.h>
-void keyboard_interrupt_handler(int sig) {
-  void *array[50];
-  size_t size;
-
-  // get void*'s for all entries on the stack
-  size = backtrace(array, 50);
-
-  // print out all the frames to stderr
-  fprintf(stderr, "Error: signal %d:\n", sig);
-  backtrace_symbols_fd(array, size, STDERR_FILENO);
-  exit(1);
-}
 
 
 vector<UInt> read_mnist_labels(string path) {
@@ -119,11 +102,13 @@ vector<UInt*> read_mnist_images(string path) {
         auto data_raw = new unsigned char[img_size];
         file.read( (char*) data_raw, img_size);
         // Copy the data into an array of UInt's
-        auto data = new UInt[2 * img_size];
+        auto data = new UInt[img_size];
+        // auto data = new UInt[2 * img_size];
         // Apply a threshold to the image, yielding a B & W image.
         for(UInt pixel = 0; pixel < img_size; pixel++) {
-            data[2 * pixel] = data_raw[pixel] >= 128 ? 1 : 0;
-            data[2 * pixel + 1] = 1 - data[2 * pixel];
+            data[pixel] = data_raw[pixel] >= 128 ? 1 : 0;
+            // data[2 * pixel] = data_raw[pixel] >= 128 ? 1 : 0;
+            // data[2 * pixel + 1] = 1 - data[2 * pixel];
         }
         retval.push_back(data);
         delete[] data_raw;
@@ -134,7 +119,7 @@ vector<UInt*> read_mnist_images(string path) {
 
 int main(int argc, char **argv) {
   UInt verbosity = 1;
-  int train_dataset_iterations = 1;
+  auto train_dataset_iterations = 1u;
   int opt;
   while ( (opt = getopt(argc, argv, "tv")) != -1 ) {  // for each option...
     switch ( opt ) {
@@ -150,9 +135,6 @@ int main(int argc, char **argv) {
     }
   }
   UInt train_time = train_dataset_iterations * 60000;
-
-  signal(SIGINT, keyboard_interrupt_handler);   // install stack trace printout on error
-  signal(SIGSEGV, keyboard_interrupt_handler);   // install stack trace printout on error
 
   SDR input({28, 28, 2});
   SpatialPooler sp(
@@ -175,25 +157,9 @@ int main(int argc, char **argv) {
     /* wrapAround */                   0 // discarded
     );
 
+
   SDR columns({sp.getNumColumns()});
   SDR_Metrics columnStats(columns, 1402);
-
-  if( verbosity ) {
-    // Print the min/mean/max potential pool size
-    UInt min = 999999;
-    Real mean = 0;
-    UInt max = 0;
-    for( UInt cell = 0; cell < sp.getNumColumns(); cell++) {
-      auto pool = sp.initMapPotential_( cell, false );
-      UInt size = 0;
-      for( auto presyn : pool ) {if ( presyn ) size++; };
-      min = size < min ? size : min;
-      max = size > max ? size : max;
-      mean += size;
-    }
-    mean /= sp.getNumColumns();
-    cerr << "POTENTIAL POOL MIN / MEAN / MAX " << min << " / " << mean << " / " << max << endl;
-  }
 
   SDRClassifier clsr(
     /* steps */         {0},
@@ -205,12 +171,19 @@ int main(int argc, char **argv) {
   auto train_images = read_mnist_images("./mnist_data/train-images-idx3-ubyte");
   auto train_labels = read_mnist_labels("./mnist_data/train-labels-idx1-ubyte");
   if(verbosity)
-    cout << "Training for " << train_time << " cycles ..." << endl;
-  for(UInt i = 0; i < train_time; i++) {
+    cout << "Training for " << (train_dataset_iterations * train_labels.size())
+         << " cycles ..." << endl;
+  for(auto i = 0u; i < train_dataset_iterations; i++) {
+    // Shuffle the training data.
+    vector<UInt> index( train_labels.size() );
+    for(auto s = 0u; s < train_labels.size(); s++)
+        index[s] = s;
+    Random().shuffle( index.begin(), index.end() );
+
+    for(auto s = 0u; s < train_labels.size(); s++) {
       // Get the input & label
-      UInt index  = rand() % train_labels.size();
-      UInt *image = train_images[index];
-      UInt label  = train_labels[index];
+      UInt *image = train_images[ index[s] ];
+      UInt label  = train_labels[ index[s] ];
 
       // Compute & Train
       input.setDense( image );
@@ -227,7 +200,7 @@ int main(int argc, char **argv) {
         cout << "." << flush;
   }
   if( verbosity ) cout << endl;
-
+  }
   cout << columnStats << endl;
 
   // Test
