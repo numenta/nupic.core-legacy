27e8ea1f575572c39e33f9fde9543c3246acbba9
ctrl-z-9000-times <dam1784@rit.edu>
2019-03-10 10:36:37 -0400

    ScalarEncoder: python bindings stubs

---
+++ bindings/py/cpp_src/bindings/encoders/encoders_module.cpp
@@ -0,0 +1,41 @@
+/* ---------------------------------------------------------------------
+ * Numenta Platform for Intelligent Computing (NuPIC)
+ * Copyright (C) 2018, chhenning
+ *               2019, David McDougall
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ *
+ * http://numenta.org/licenses/
+ * --------------------------------------------------------------------- */
+
+/** @file
+ * Encoders bindings Module file for pybind11
+ */
+
+#include <bindings/suppress_register.hpp>  //must be before pybind11.h
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+namespace nupic_ext
+{
+    void init_ScalarEncoder(py::module&);
+}
+
+using namespace nupic_ext;
+
+PYBIND11_MODULE(encoders, m) {
+    m.doc() = "nupic.bindings.encoders"; // optional module docstring
+
+    init_ScalarEncoder(m);
+}
---
+++ bindings/py/cpp_src/bindings/encoders/py_ScalarEncoder.cpp
@@ -0,0 +1,37 @@
+/* ----------------------------------------------------------------------
+ * Numenta Platform for Intelligent Computing (NuPIC)
+ * Copyright (C) 2019, David McDougall
+ * The following terms and conditions apply:
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ *
+ * http://numenta.org/licenses/
+ * ---------------------------------------------------------------------- */
+
+#include <bindings/suppress_register.hpp>  //include before pybind11.h
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <pybind11/stl.h>
+namespace py = pybind11;
+
+#include <nupic/encoders/ScalarEncoder.hpp>
+
+using namespace nupic;
+
+namespace nupic_ext
+{
+    void init_ScalarEncoder(py::module& m)
+    {
+        py::class_<SDR> py_ScalarEncoder(m, "ScalarEncoder", "TODO DOC");
+    }
+}
