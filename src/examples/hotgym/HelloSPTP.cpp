efaba0536865053457ae89070aed25e49ce6960c
Marek Otahal <markotahal@gmail.com>
2019-04-18 08:19:52 +0200

    Hotgym: fix the example runtime
    use seed for RDSE,
    SDR use addNoise instead of randomize

--- src/examples/hotgym/HelloSPTP.cpp
+++ src/examples/hotgym/HelloSPTP.cpp
@@ -66,9 +66,11 @@ Real64 BenchmarkHotgym::run(UInt EPOCHS, bool useSPlocal, bool useSPglobal, bool
   // initialize SP, TM, Anomaly, AnomalyLikelihood
   tInit.start();
   EncoderParameters encParams;
-  encParams.sparsity = 0.3f; //30% of the encoding are active bits (1's)
+  encParams.sparsity = 0.2f; //20% of the encoding are active bits (1's)
   encParams.size = DIM_INPUT; //the encoder is not optimal, it's to stress-test the SP,TM
-  encParams.resolution = 0.002f;
+//  encParams.resolution = 0.002f;
+  encParams.radius = 0.03f;
+  encParams.seed = 2019u;
   Encoder enc( encParams );
   SpatialPooler spGlobal(enc.dimensions, vector<UInt>{COLS}); // Spatial pooler with globalInh
   SpatialPooler  spLocal(enc.dimensions, vector<UInt>{COLS}); // Spatial pooler with local inh
@@ -102,10 +104,11 @@ Real64 BenchmarkHotgym::run(UInt EPOCHS, bool useSPlocal, bool useSPglobal, bool
     tEnc.start();
     x+=0.01f; //step size for fn(x)
     enc.encode(sin(x), input); //model sin(x) function //TODO replace with CSV data
+//    cout << x << "\n" << sin(x) << "\n" << input << "\n\n";
     tEnc.stop();
 
     tRng.start();
-    input.randomize(0.01, rnd); //change 1% of the SDR for each iteration, this makes a random sequence, but seemingly stable
+    input.addNoise(0.01, rnd); //change 1% of the SDR for each iteration, this makes a random sequence, but seemingly stable
     tRng.stop();
 
     //SP (global x local)
@@ -166,34 +169,37 @@ Real64 BenchmarkHotgym::run(UInt EPOCHS, bool useSPlocal, bool useSPglobal, bool
       // check deterministic SP, TM output 
       SDR goldEnc({DIM_INPUT});
       const SDR_sparse_t deterministicEnc{
-        227, 246, 362, 704, 726, 1010, 1040, 1100, 1117, 1240, 1338, 1366, 1373, 1395, 1433, 1611, 1682, 1693, 1700, 1877, 1926, 2112, 2139, 2345, 2450, 2533, 2580, 2649, 2658, 2973, 2998, 3084, 3096, 3235, 3376, 3425, 3465, 3740, 4085, 4147, 4181, 4306, 4645, 4662, 4680, 4871, 4941, 4958, 4986, 4999, 5085, 5153, 5248, 5285, 5386, 5424, 5736, 5816, 5832, 5851, 5945, 6026, 6028, 6104, 6182, 6206, 6207, 6253, 6475, 6571, 6604, 6900, 6916, 6928, 6938, 7026, 7115, 7139, 7417, 7533, 7682, 7715, 7821, 7868, 7999, 8033, 8270, 8641, 8706, 9019, 9138, 9217, 9350, 9469, 9547, 9589, 9623, 9828, 9869, 9899
+        0, 4, 13, 21, 24, 30, 32, 37, 40, 46, 47, 48, 50, 51, 64, 68, 79, 81, 89, 97, 99, 114, 120, 135, 136, 140, 141, 143, 144, 147, 151, 155, 161, 162, 164, 165, 169, 172, 174, 179, 181, 192, 201, 204, 205, 210, 213, 226, 237, 242, 247, 249, 254, 255, 262, 268, 271, 282, 283, 295, 302, 306, 307, 317, 330, 349, 353, 366, 368, 380, 393, 399, 404, 409, 410, 420, 422, 441,446, 447, 456, 458, 464, 468, 476, 497, 499, 512, 521, 528, 531, 534, 538, 539, 541, 545, 550, 557, 562, 565, 575, 581, 589, 592, 599, 613, 617, 622, 647, 652, 686, 687, 691, 699, 704, 710, 713, 716, 722, 729, 736, 740, 747, 749, 753, 754, 758, 766, 778, 790, 791, 797, 800, 808, 809, 812, 815, 826, 828, 830, 837, 838, 852, 853, 856, 863, 864, 873, 878, 882, 885, 893, 894, 895, 905, 906, 914, 915, 920, 924, 927, 937, 939, 944, 947, 951, 954, 956, 967, 968, 969, 973, 975, 976, 981, 991, 998
       };
       goldEnc.setSparse(deterministicEnc);
 
       SDR goldSP({COLS});
       const SDR_sparse_t deterministicSP{
-        253, 277, 502, 542, 701, 1011, 1268, 1459, 1783, 1870
+        72, 75, 284, 303, 305, 317, 329, 525, 1095, 2027
       };
       goldSP.setSparse(deterministicSP);
 
       SDR goldSPlocal({COLS});
-      const SDR_sparse_t deterministicSPlocal{}; //FIXME SP local inh returns almost all active cols, probably incorrect settings
+      const SDR_sparse_t deterministicSPlocal{
+        6, 12, 26, 57, 63, 72, 75, 76, 77, 80, 82, 103, 105, 124, 135, 154, 171, 174, 175, 185, 192, 193, 195, 198, 263, 284, 296, 302, 303, 305, 313, 317, 319, 320, 356, 363, 364, 401, 403, 404, 410, 413, 425, 426, 428, 449, 491, 496, 511, 515, 516, 518, 520, 525, 529, 536, 550, 556, 574, 583, 592, 597, 598, 603, 609, 622, 626, 636, 645, 652, 704, 706, 722, 726, 727, 728, 729, 747, 751, 766, 779, 808, 833, 837, 838, 840, 848, 850, 853, 860, 908, 912, 918, 919, 923, 927, 929, 930, 931, 932, 970, 989, 1006, 1038, 1066, 1082, 1085, 1087, 1092, 1094, 1095, 1113, 1115, 1125, 1128, 1174, 1179, 1180, 1182, 1185, 1205, 1206, 1232, 1236, 1238, 1239, 1240, 1245, 1271, 1292, 1295, 1300, 1303, 1307, 1311, 1319, 1320, 1322, 1382, 1401, 1412, 1415, 1421, 1426, 1431, 1434, 1438, 1470, 1474, 1492, 1501, 1511, 1521, 1524, 1525, 1530, 1532, 1537, 1540, 1600, 1617, 1620, 1622, 1632, 1638, 1641, 1667, 1672, 1680, 1684, 1686, 1690, 1699, 1702, 1742, 1744, 1745, 1746, 1765, 1770, 1774, 1801, 1807, 1808, 1816, 1830, 1834, 1849, 1861, 1867, 1871, 1882, 1902, 1907, 1943, 1945, 1955, 1956, 1966, 1968, 1969, 1971, 1986, 2018, 2025, 2027
+      };
       goldSPlocal.setSparse(deterministicSPlocal);
 
       SDR goldTM({COLS});
       const SDR_sparse_t deterministicTM{
-      
-      }; //FIXME why TM does not learn, and this field is empty?!
+        26, 75 
+      };
       goldTM.setSparse(deterministicTM);
 
-      const float goldAn = 1.0f;
+      const float goldAn = 0.920001f;
 
       if(EPOCHS == 5000) { //these hand-written values are only valid for EPOCHS = 5000 (default), but not for debug and custom runs. 
         NTA_CHECK(input == goldEnc) << "Deterministic output of Encoder failed!\n" << input << "should be:\n" << goldEnc;
         NTA_CHECK(outSP == goldSP) << "Deterministic output of SP (g) failed!\n" << outSP << "should be:\n" << goldSP;
-	//TODO enable the check: NTA_CHECK(outSPlocal == goldSPlocal) << "Deterministic output of SP (l) failed!\n" << outSPlocal << "should be:\n" << goldSPlocal;
+	NTA_CHECK(outSPlocal == goldSPlocal) << "Deterministic output of SP (l) failed!\n" << outSPlocal << "should be:\n" << goldSPlocal;
         NTA_CHECK(outTM == goldTM) << "Deterministic output of TM failed!\n" << outTM << "should be:\n" << goldTM; 
-        NTA_CHECK(res == goldAn) << "Deterministic output of Anomaly failed! " << res << "should be: " << goldAn;
+        NTA_CHECK(static_cast<UInt>(res *10000) == static_cast<UInt>(goldAn *10000)) //compare to 4 decimal places
+		<< "Deterministic output of Anomaly failed! " << res << "should be: " << goldAn;
       }
 
       // check runtime speed
