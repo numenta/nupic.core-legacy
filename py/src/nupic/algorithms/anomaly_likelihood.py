5c60fa6b622c1e2ff6abae081f3983f2ad49381f
Marek Otahal <markotahal@gmail.com>
2019-05-08 15:22:14 +0200

    Python AnomalyLikelihood: remove capnp Serializable
    as it's not supported

--- py/src/nupic/algorithms/anomaly_likelihood.py
+++ py/src/nupic/algorithms/anomaly_likelihood.py
@@ -119,17 +119,9 @@ import math
 import numbers
 import numpy
 
-from nupic.serializable import Serializable
 from nupic.utils import MovingAverage
 
-try:
-  import capnp
-except ImportError:
-  capnp = None
-if capnp:
-  from nupic.algorithms.anomaly_likelihood_capnp import AnomalyLikelihoodProto
-
-class AnomalyLikelihood(Serializable):
+class AnomalyLikelihood:
   """
   Helper class for running anomaly likelihood computation. To use it simply
   create an instance and then feed it successive anomaly scores:
@@ -256,102 +248,6 @@ class AnomalyLikelihood(Serializable):
     """
     numShiftedOut = max(0, numIngested - windowSize)
     return min(numIngested, max(0, learningPeriod - numShiftedOut))
-
-
-  @classmethod
-  def getSchema(cls):
-    return AnomalyLikelihoodProto
-
-  @classmethod
-  def read(cls, proto):
-    """ capnp deserialization method for the anomaly likelihood object
-
-    :param proto: (Object) capnp proto object specified in
-                          nupic.regions.anomaly_likelihood.capnp
-
-    :returns: (Object) the deserialized AnomalyLikelihood object
-    """
-    # pylint: disable=W0212
-    anomalyLikelihood = object.__new__(cls)
-    anomalyLikelihood._iteration = proto.iteration
-
-    anomalyLikelihood._historicalScores = collections.deque(
-      maxlen=proto.historicWindowSize)
-    for i, score in enumerate(proto.historicalScores):
-      anomalyLikelihood._historicalScores.append((i, score.value,
-                                                  score.anomalyScore))
-    if proto.distribution.name: # is "" when there is no distribution.
-      anomalyLikelihood._distribution = dict()
-      anomalyLikelihood._distribution['distribution'] = dict()
-      anomalyLikelihood._distribution['distribution']["name"] = proto.distribution.name
-      anomalyLikelihood._distribution['distribution']["mean"] = proto.distribution.mean
-      anomalyLikelihood._distribution['distribution']["variance"] = proto.distribution.variance
-      anomalyLikelihood._distribution['distribution']["stdev"] = proto.distribution.stdev
-
-      anomalyLikelihood._distribution["movingAverage"] = {}
-      anomalyLikelihood._distribution["movingAverage"]["windowSize"] = proto.distribution.movingAverage.windowSize
-      anomalyLikelihood._distribution["movingAverage"]["historicalValues"] = []
-      for value in proto.distribution.movingAverage.historicalValues:
-        anomalyLikelihood._distribution["movingAverage"]["historicalValues"].append(value)
-      anomalyLikelihood._distribution["movingAverage"]["total"] = proto.distribution.movingAverage.total
-
-      anomalyLikelihood._distribution["historicalLikelihoods"] = []
-      for likelihood in proto.distribution.historicalLikelihoods:
-        anomalyLikelihood._distribution["historicalLikelihoods"].append(likelihood)
-    else:
-      anomalyLikelihood._distribution = None
-
-    anomalyLikelihood._probationaryPeriod = proto.probationaryPeriod
-    anomalyLikelihood._learningPeriod = proto.learningPeriod
-    anomalyLikelihood._reestimationPeriod = proto.reestimationPeriod
-    # pylint: enable=W0212
-
-    return anomalyLikelihood
-
-
-  def write(self, proto):
-    """ capnp serialization method for the anomaly likelihood object
-
-    :param proto: (Object) capnp proto object specified in
-                          nupic.regions.anomaly_likelihood.capnp
-    """
-
-    proto.iteration = self._iteration
-
-    pHistScores = proto.init('historicalScores', len(self._historicalScores))
-    for i, score in enumerate(list(self._historicalScores)):
-      _, value, anomalyScore = score
-      record = pHistScores[i]
-      record.value = float(value)
-      record.anomalyScore = float(anomalyScore)
-
-    if self._distribution:
-      proto.distribution.name = self._distribution["distribution"]["name"]
-      proto.distribution.mean = float(self._distribution["distribution"]["mean"])
-      proto.distribution.variance = float(self._distribution["distribution"]["variance"])
-      proto.distribution.stdev = float(self._distribution["distribution"]["stdev"])
-
-      proto.distribution.movingAverage.windowSize = float(self._distribution["movingAverage"]["windowSize"])
-
-      historicalValues = self._distribution["movingAverage"]["historicalValues"]
-      pHistValues = proto.distribution.movingAverage.init(
-        "historicalValues", len(historicalValues))
-      for i, value in enumerate(historicalValues):
-        pHistValues[i] = float(value)
-
-      #proto.distribution.movingAverage.historicalValues = self._distribution["movingAverage"]["historicalValues"]
-      proto.distribution.movingAverage.total = float(self._distribution["movingAverage"]["total"])
-
-      historicalLikelihoods = self._distribution["historicalLikelihoods"]
-      pHistLikelihoods = proto.distribution.init("historicalLikelihoods",
-                                                 len(historicalLikelihoods))
-      for i, likelihood in enumerate(historicalLikelihoods):
-        pHistLikelihoods[i] = float(likelihood)
-
-    proto.probationaryPeriod = self._probationaryPeriod
-    proto.learningPeriod = self._learningPeriod
-    proto.reestimationPeriod = self._reestimationPeriod
-    proto.historicWindowSize = self._historicalScores.maxlen
 
 
   def anomalyProbability(self, value, anomalyScore, timestamp=None):
