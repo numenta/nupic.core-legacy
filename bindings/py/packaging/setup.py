6ec720abf1faf991535c584d1edc7c0891cde747
David Keeney <dkeeney@gmail.com>
2019-08-11 12:47:07 -0700

    CircleCI build C++ in debug and run unit_test, then build everything in release for distribution.

--- bindings/py/packaging/setup.py
+++ bindings/py/packaging/setup.py
@@ -29,10 +29,33 @@ from setuptools import Command, find_packages, setup
 from setuptools.command.test import test as BaseTestCommand
 from distutils.core import Extension
 
+# NOTE:  You can request a DEBUG version of the build by setting the environment
+#        variable CMAKE_BUILD_TYPE=Debug  prior to running this script.
+#        However this requires a debug version of the python library which may not be available.
+#        An alternative way to debug the python bindings is to use the procedure
+#        described here: https://pythonextensionpatterns.readthedocs.io/en/latest/debugging/debug_in_ide.html
+#
+
+# NOTE:  CMake usually is able to determine the tool chain based on the platform.
+#        However, if you would like CMake to use a different generator, (perhaps an 
+#        alternative compiler) you can set the environment variable NC_CMAKE_GENERATOR
+#        to the generator you wish to use.  See CMake docs for generators avaiable.
+#         
+#         On Windows, CMake will try to use the newest Visual Studio installed
+#         on your machine. You many choose an older version as follows:
+#            set NC_CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
+#            python setup.py install --user --force
+#         This script will override the default 32bit platform such that a 64bit build is created.
+#
+
+
+build_type = os.environ.get('CMAKE_BUILD_TYPE')
+if build_type == None: build_type = 'Release'
+
 
 PY_BINDINGS = os.path.dirname(os.path.realpath(__file__))
 REPO_DIR = os.path.abspath(os.path.join(PY_BINDINGS, os.pardir, os.pardir, os.pardir))
-DISTR_DIR = os.path.join(REPO_DIR, "build", "Release", "distr")
+DISTR_DIR = os.path.join(REPO_DIR, "build", build_type, "distr")
 DARWIN_PLATFORM = "darwin"
 LINUX_PLATFORM = "linux"
 UNIX_PLATFORMS = [LINUX_PLATFORM, DARWIN_PLATFORM]
@@ -153,7 +176,7 @@ def getPlatformInfo():
 
 
 
-def getExtensionFileNames(platform):
+def getExtensionFileNames(platform, build_type):
   # look for extension libraries in Repository/build/Release/distr/src/htm/bindings
   # library filenames:  
   #     htm.core.algorithms.so
@@ -178,11 +201,11 @@ def getExtensionFileNames(platform):
   return files
 
 
-def getExtensionFiles(platform):
-  files = getExtensionFileNames(platform)
+def getExtensionFiles(platform, build_type):
+  files = getExtensionFileNames(platform, build_type)
   for f in files:
     if not glob.glob(f):
-      generateExtensions(platform)
+      generateExtensions(platform, build_type)
       break
 
   return files
@@ -204,7 +227,7 @@ def isMSVC_installed(ver):
   return False
 
 
-def generateExtensions(platform):
+def generateExtensions(platform, build_type):
   """
   This will perform a full Release build with default arguments.
   The CMake build will copy everything in the Repository/bindings/py/packaging 
@@ -214,6 +237,7 @@ def generateExtensions(platform):
   """
   cwd = os.getcwd()
   
+  print("Python version: {}\n".format(sys.version))
   from sys import version_info
   if version_info > (3, 0):
     # Build a Python 3.x library
@@ -223,8 +247,10 @@ def generateExtensions(platform):
     PY_VER = "-DBINDING_BUILD=Python2"
     if platform == "windows":
         raise Exception("Python2 is not supported on Windows.")
+        
 
-  print("Python version: {}\n".format(sys.version))
+  BUILD_TYPE = "-DCMAKE_BUILD_TYPE="+build_type
+
 
   scriptsDir = os.path.join(REPO_DIR, "build", "scripts")
   try:
@@ -245,16 +271,16 @@ def generateExtensions(platform):
         if not os.path.isfile('CMakeCache.txt') or not 'BINDING_BUILD:STRING=Python3' in open('CMakeCache.txt').read():
           # Note: the calling arguments for MSVC 2017 is not the same as for MSVC 2019
           if isMSVC_installed("2019"):
-            subprocess.check_call(["cmake", "-G", "Visual Studio 16 2019", "-A", "x64", PY_VER, REPO_DIR])
+            subprocess.check_call(["cmake", "-G", "Visual Studio 16 2019", "-A", "x64", BUILD_TYPE, PY_VER, REPO_DIR])
           elif isMSVC_installed("2017"):
-            subprocess.check_call(["cmake", "-G", "Visual Studio 15 2017 Win64", PY_VER, REPO_DIR])
+            subprocess.check_call(["cmake", "-G", "Visual Studio 15 2017 Win64", BUILD_TYPE, PY_VER, REPO_DIR])
           else:
             raise Exception("Did not find Microsoft Visual Studio 2017 or 2019.")
         #else 
         #   we can skip this step, the cache is already setup and we have the right binding specified.
       else:
         # For Linux and OSx we can let CMake figure it out.
-        subprocess.check_call(["cmake", PY_VER, REPO_DIR])
+        subprocess.check_call(["cmake",BUILD_TYPE , PY_VER, REPO_DIR])
         
     else:
       # The generator is specified.
@@ -263,16 +289,16 @@ def generateExtensions(platform):
         if not os.path.isfile("CMakeCache.txt"):
           # Note: the calling arguments for MSVC 2017 is not the same as for MSVC 2019
           if '2019' in generator and isMSVC_installed("2019"):
-            subprocess.check_call(["cmake", "-G", "Visual Studio 16 2019", "-A", "x64", PY_VER, REPO_DIR])
+            subprocess.check_call(["cmake", "-G", "Visual Studio 16 2019", "-A", "x64", BUILD_TYPE, PY_VER, REPO_DIR])
           elif '2017' in generator and isMSVC_installed("2017"):
-            subprocess.check_call(["cmake", "-G", "Visual Studio 15 2017 Win64", PY_VER, REPO_DIR])
+            subprocess.check_call(["cmake", "-G", "Visual Studio 15 2017 Win64", BUILD_TYPE, PY_VER, REPO_DIR])
           else:
             raise Exception('Did not find Visual Studio for generator "'+generator+ '".')
       else:
-        subprocess.check_call(["cmake", "-G", generator, PY_VER, REPO_DIR])
+        subprocess.check_call(["cmake", "-G", generator, BUILD_TYPE, PY_VER, REPO_DIR])
         
     # Now do `make install`
-    subprocess.check_call(["cmake", "--build", ".", "--target", "install", "--config", "Release"])
+    subprocess.check_call(["cmake", "--build", ".", "--target", "install", "--config", build_type])
   finally:
     os.chdir(cwd)
 
@@ -286,20 +312,15 @@ if __name__ == "__main__":
                     "`export ARCHFLAGS=\"-arch x86_64\"`.")
 
   # Run CMake if extension files are missing.
-  getExtensionFiles(platform)
+  # CMake also copies all py files into place in DISTR_DIR
+  getExtensionFiles(platform, build_type)
 
-  # Copy the packaging into place (from bindings/py/packaging/ to build/Release/distro)
-  distutils.dir_util.copy_tree(PY_BINDINGS, DISTR_DIR)
-
-  # Copy the python code into place. (from /py/htm/ to build/Release/distro/src/htm)
-  distutils.dir_util.copy_tree(
-            os.path.join(REPO_DIR, "py", "htm"), os.path.join(DISTR_DIR, "src", "htm"))
   """
   set the default directory to the distr, and package it.
   """
   os.chdir(DISTR_DIR)
 
-  print("\nbindings/py/setup.py: Setup Pybind11 Python module in " + DISTR_DIR+ "\n")
+  print("\nbindings/py/setup.py: Setup htm.core Python module in " + DISTR_DIR+ "\n")
   setup(
     # See https://docs.python.org/2/distutils/apiref.html for descriptions of arguments.
     #     https://docs.python.org/2/distutils/setupscript.html
