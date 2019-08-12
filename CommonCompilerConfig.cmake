d76c5b2689b900cc9aabd4acadca65a105372bd2
Marek Otahal <markotahal@gmail.com>
2019-08-12 22:33:23 +0200

    XCode: until macOS 10.15, we need to build with c++11

--- CommonCompilerConfig.cmake
+++ CommonCompilerConfig.cmake
@@ -109,11 +109,12 @@ if(NOT FORCE_CPP11)
     endif()	 
   elseif(${CMAKE_CXX_COMPILER_ID} MATCHES "AppleClang")  # see CMake Policy CMP0025
     if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "11") # XCode 11 & AppleClang 11 do support c++17 with <filesystem>
-      ## TODO XCode11 on macOS 10.15 supports <filesystem>,
+      ## TODO XCode11 on macOS 10.15 supports c++17 and <filesystem>,
       # https://developer.apple.com/documentation/xcode_release_notes/xcode_11_beta_5_release_notes
       # but for now CircleCI uses macOS 10.14, so we cannot disable boost yet.
-      # set(boost_required OFF)
-      # set(CMAKE_CXX_STANDARD 17)
+      # macOS 10.15 will be release in Sept 2019, so we can switch to it soon after it.
+      set(boost_required ON)
+      set(CMAKE_CXX_STANDARD 11)
     endif()
   # does not support C++17 and filesystem (as of XCode 10.1)
   elseif(${CMAKE_CXX_COMPILER_ID} MATCHES "Clang") # clang + std::filesystem, see https://libcxx.llvm.org/docs/UsingLibcxx.html#using-filesystem
