06526cdd2f572642a90870b9e95cd3dddd4754e6
David Keeney <dkeeney@gmail.com>
2019-08-13 17:22:00 -0700

    corrected the PATCH part of version.  had a typo.

--- versioning.cmake
+++ versioning.cmake
@@ -43,76 +43,77 @@
 
 function(get_versions version major minor patch)
     # do we have Git installed and are we in a repository?
-	find_package(Git)
-	if(GIT_FOUND)
-		execute_process(COMMAND "${GIT_EXECUTABLE}" log --pretty=format:'%h' -n 1
-	        OUTPUT_VARIABLE GIT_REV
-			OUTPUT_STRIP_TRAILING_WHITESPACE
-	        ERROR_QUIET)
-	endif()
-	# Check whether we got any revision; which isn't
-	# always the case, e.g. when someone downloaded a tar.gz or zip
-	# file from Github instead of a checkout...
+    find_package(Git)
+    if(GIT_FOUND)
+        execute_process(COMMAND "${GIT_EXECUTABLE}" log --pretty=format:'%h' -n 1
+            OUTPUT_VARIABLE GIT_REV
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            ERROR_QUIET)
+    endif()
+    # Check whether we got any revision; which isn't
+    # always the case, e.g. when someone downloaded a tar.gz or zip
+    # file from Github instead of a checkout...
     # or maybe git is not even installed.
-	if ("${GIT_REV}" STREQUAL "")
-	    # use the version already in the VERSION file.
-	    if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/VERSION)
-	       file(READ ${CMAKE_CURRENT_SOURCE_DIR}/VERSION GIT_TAG)
-	    endif()
-	else()
-	    
-	    # see if there as a tag set on this PR
-	    execute_process(
-	        COMMAND "${GIT_EXECUTABLE}" describe --exact-match --tags
-	        OUTPUT_VARIABLE GIT_TAG 
-		    OUTPUT_STRIP_TRAILING_WHITESPACE
-		    ERROR_QUIET)
+    if ("${GIT_REV}" STREQUAL "")
+        # use the version already in the VERSION file.
+        if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/VERSION)
+           file(READ ${CMAKE_CURRENT_SOURCE_DIR}/VERSION GIT_TAG)
+        endif()
+    else()
+        
+        # see if there as a tag set on this PR
+        execute_process(
+            COMMAND "${GIT_EXECUTABLE}" describe --exact-match --tags
+            OUTPUT_VARIABLE GIT_TAG 
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            ERROR_QUIET)
     
-	    if ("${GIT_TAG}" STREQUAL "")
-	        execute_process(
-	            COMMAND "${GIT_EXECUTABLE}" describe --tags
-	            OUTPUT_VARIABLE GIT_TAG 
-		        OUTPUT_STRIP_TRAILING_WHITESPACE
-		        ERROR_QUIET)
-	    endif()
+        if ("${GIT_TAG}" STREQUAL "")
+            execute_process(
+                COMMAND "${GIT_EXECUTABLE}" describe --tags
+                OUTPUT_VARIABLE GIT_TAG 
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+                ERROR_QUIET)
+        endif()
     endif()
     
     if ("${GIT_TAG}" STREQUAL "")
         set(m_version ${GIT_REV})
-		set(${version} ${m_version} PARENT_SCOPE)
-		set(${major}   0 PARENT_SCOPE)
-		set(${minor}   0 PARENT_SCOPE)
-		set(${patch}   0 PARENT_SCOPE)
-		return()
+        set(${version} ${m_version} PARENT_SCOPE)
+        set(${major}   0 PARENT_SCOPE)
+        set(${minor}   0 PARENT_SCOPE)
+        set(${patch}   0 PARENT_SCOPE)
+        return()
     else()
         set(m_version ${GIT_TAG})
     endif()
 
+    message(STATUS "Version from Git: ${m_version}")
    
-	string(REGEX REPLACE "^([vV])([0-9]*)([.][0-9]*[.][0-9]*-?.*)$" "\\2" numbers ${m_version} )
-	set(m_major ${numbers})
-	string(REGEX REPLACE "^([vV][0-9]*[.])([0-9]*)([.][0-9]*-?.*)$" "\\2" numbers ${m_version} )
-	set(m_minor ${numbers})
-	string(REGEX REPLACE "^([vV][0-9]*[.][0-9]*[.])([0-9]*)(-?.*)$" "\\2" numbers ${m_version} )
-	set(m_patch ${numbers})
-	
+    string(REGEX REPLACE "^([vV])([0-9]*)([.][0-9]*[.][0-9]*-?.*)$" "\\2" numbers ${m_version} )
+    set(m_major ${numbers})
+    string(REGEX REPLACE "^([vV][0-9]*[.])([0-9]*)([.][0-9]*-?.*)$" "\\2" numbers ${m_version} )
+    set(m_minor ${numbers})
+    string(REGEX REPLACE "^([vV][0-9]*[.][0-9]*[.])([0-9]*)(-?.*)$" "\\2" numbers ${m_version} )
+    set(m_patch ${numbers})
+    
     # limiting ourselves to Major.Minor.Patch format for version.
-    set(m_version "${m_major}.${m_minor}.${patch}")
+    set(m_version "${m_major}.${m_minor}.${m_patch}")
 
-	# Write it to the file VERSION if it is different.
-	if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/VERSION)
-	    file(READ ${CMAKE_CURRENT_SOURCE_DIR}/VERSION m_version_)
-	else()
-	    set(m_version_ "")
-	endif()
+    # Write it to the file VERSION if it is different.
+    if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/VERSION)
+        file(READ ${CMAKE_CURRENT_SOURCE_DIR}/VERSION m_version_)
+    else()
+        set(m_version_ "")
+    endif()
 
-	if (NOT "${m_version}" STREQUAL "${m_version_}")
-	    file(WRITE ${CMAKE_CURRENT_SOURCE_DIR}/VERSION "${m_version}")
-	endif()
-	
-	# return the arguments
-	set(${version} ${m_version} PARENT_SCOPE)
-	set(${major}   ${m_major} PARENT_SCOPE)
-	set(${minor}   ${m_minor} PARENT_SCOPE)
-	set(${patch}   ${m_patch} PARENT_SCOPE)
+    if (NOT "${m_version}" STREQUAL "${m_version_}")
+        file(WRITE ${CMAKE_CURRENT_SOURCE_DIR}/VERSION "${m_version}")
+    endif()
+    
+    # return the arguments
+    set(${version} ${m_version} PARENT_SCOPE)
+    set(${major}   ${m_major} PARENT_SCOPE)
+    set(${minor}   ${m_minor} PARENT_SCOPE)
+    set(${patch}   ${m_patch} PARENT_SCOPE)
 endfunction()
