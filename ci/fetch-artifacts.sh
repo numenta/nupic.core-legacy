6a4d99114eb40bf3f85e62722cc274679c9bc042
David Keeney <dkeeney@gmail.com>
2019-08-17 11:20:45 -0700

    trying to get artifacts with correct name

--- ci/fetch-artifacts.sh
+++ ci/fetch-artifacts.sh
@@ -2,6 +2,12 @@
 
 # script to fetch artifacts from Win,OSX CI to Travis,
 # where all 3 files are deployed to GitHub for release. 
+# curent pwd should be build/scripts
+
+
+echo "Released version: "
+cat ../../VERSION
+echo
 
 WINDOWS_ARTIFACT_URL="https://ci.appveyor.com/api/buildjobs/by4cpqmul79tp9ag/artifacts/build/scripts/htm_core-6866bd13ece35608313709e82c29b2260ee7be73-windows64.zip" #FIXME make the URLs automated, needs API?
 OSX_ARTIFACT_URL="https://3238-118031881-gh.circle-artifacts.com/0/tmp/uploads/htm_core-7b7b53c798b3795088ce772e96c6994068fc1302-darwin64.tar.gz"
@@ -38,12 +44,16 @@ for i in `seq 10`; do
   fi
 done
 
+
 # extract the wheel files for deploying to PYPI
 mkdir artifacts
-unzip *.zip -d artifacts
 cd artifacts
-tar -xf ../*.gz
+unzip ../*windows64.zip
+tar -xvf ../*linux64.tar.gz
+tar -xvf ../*darwin64.tar.gz
 cp */py/*.whl ..
 cd ..
+echo "wheels ready for deployment"
+ls *.whl
 #rm -r artifacts
 
--- versioning.cmake
+++ versioning.cmake
@@ -57,11 +57,12 @@ function(get_versions version major minor patch)
     if ("${GIT_REV}" STREQUAL "")
         # use the version already in the VERSION file.
         if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/VERSION)
+           message(STATUS "NOTE: USING version from VERSION file. No git.")
            file(READ ${CMAKE_CURRENT_SOURCE_DIR}/VERSION GIT_TAG)
         endif()
     else()
         
-        # see if there as a tag set on this PR
+        # see if there as a tag set on this branch
         execute_process(
             COMMAND "${GIT_EXECUTABLE}" describe --exact-match --tags
             OUTPUT_VARIABLE GIT_TAG 
@@ -75,20 +76,31 @@ function(get_versions version major minor patch)
                 OUTPUT_STRIP_TRAILING_WHITESPACE
                 ERROR_QUIET)
         endif()
+        
+        if ("${GIT_TAG}" STREQUAL "")
+           # use the version already in the VERSION file.
+           # This could happen if not enough of the repository is downloaded.
+           # i.e. use "git fetch --depth=200"
+           if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/VERSION)
+              message(STATUS "NOTE: USING version from VERSION file. No git.")
+              file(READ ${CMAKE_CURRENT_SOURCE_DIR}/VERSION GIT_TAG)
+           endif()
+        endif()
     endif()
     
     if ("${GIT_TAG}" STREQUAL "")
+        # even the VERSION file must have been empty...should not happen.
         set(m_version ${GIT_REV})
         set(${version} ${m_version} PARENT_SCOPE)
         set(${major}   0 PARENT_SCOPE)
         set(${minor}   0 PARENT_SCOPE)
         set(${patch}   0 PARENT_SCOPE)
         return()
-    else()
-        set(m_version ${GIT_TAG})
     endif()
 
-    message(STATUS "Version from Git: ${m_version}")
+    set(m_version ${GIT_TAG})
+
+    message(STATUS "Full Version from Git: ${m_version}")
    
     string(REGEX REPLACE "^([vV])([0-9]*)([.][0-9]*[.][0-9]*-?.*)$" "\\2" numbers ${m_version} )
     set(m_major ${numbers})
@@ -97,8 +109,8 @@ function(get_versions version major minor patch)
     string(REGEX REPLACE "^([vV][0-9]*[.][0-9]*[.])([0-9]*)(-?.*)$" "\\2" numbers ${m_version} )
     set(m_patch ${numbers})
     
-    # limiting ourselves to Major.Minor.Patch format for version.
-    set(m_version "${m_major}.${m_minor}.${m_patch}")
+    # limiting ourselves to v Major.Minor.Patch format for version.
+    set(m_version "v${m_major}.${m_minor}.${m_patch}")
 
     # Write it to the file VERSION if it is different.
     if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/VERSION)
