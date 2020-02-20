d3c51d6ead4b071700e6bdc8e8eef5c1788f83e4
Marek Otahal <markotahal@gmail.com>
2020-02-20 16:33:45 +0100

    code cleanup

--- src/htm/algorithms/SpatialPooler.cpp
+++ src/htm/algorithms/SpatialPooler.cpp
@@ -866,22 +866,22 @@ void SpatialPooler::inhibitColumnsLocal_(const vector<Real> &overlaps,
 
 
       if (wrapAround_) {
-        numNeighbors = 0;  // In wrapAround, number of neighbors to be cons     idered is solely a function of the inhibition radius, the number of dimensi     ons, and of the size of each of those dimenion
+         numNeighbors = 0;  // In wrapAround, number of neighbors to be considered is solely a function of the inhibition radius, 
+	 // ... the number of dimensions, and of the size of each of those dimenion
          UInt predN = 1;
-         for (UInt i = 0; i<columnDimensions_.size();i++) {
-           UInt diam = 2*inhibitionRadius_ + 1;
-           predN *= ( diam + ((columnDimensions_[i] - diam) * ((UInt) (columnDimensions_[i] - diam) >> (shftInt))) ); // max(diam, columDimenions_[i]);
+         for (auto dim : columnDimensions_) {
+           const UInt diam = 2*inhibitionRadius_ + 1; //the inh radius can change, that's why we recompute here
+           predN *= ( diam + ((dim - diam) * static_cast<UInt>((dim - diam) >> (shftInt))) ); // max(diam, columDimenions_[i]);
          }
          predN -= 1;
          numNeighbors = predN;
-         const UInt numActive_wrap = (UInt)(0.5f + (density * (numNeighbors + 1)));
+         const UInt numActive_wrap = static_cast<UInt>(0.5f + (density * (numNeighbors + 1)));
 
         for(auto neighbor: WrappingNeighborhood(column, inhibitionRadius_,columnDimensions_)) { //TODO if we don't change inh radius (changes only every isUpdateRound()),
 		// then these values can be cached -> faster local inh
           if (neighbor == column) {
             continue;
           }
-//          numNeighbors++;
 
           const Real difference = overlaps[neighbor] - overlaps[column];
           if (difference > 0 || (difference == 0 && activeColumnsDense[neighbor])) {
